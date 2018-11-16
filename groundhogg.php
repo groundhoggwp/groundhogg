7a119a2291ab9c27f61ad786c92bb0f544ec2a10
tobeyadr <gO0UsDYxRwhz>
2018-11-16 08:32:34 -0500

    update version

--- groundhogg.php
+++ groundhogg.php
@@ -3,7 +3,7 @@
 Plugin Name: Groundhogg
 Plugin URI: https://wordpress.org/plugins/groundhogg/
 Description: CRM and marketing automation for WordPress
-Version: 1.0.9
+Version: 1.0.9.1
 Author: Groundhogg Inc.
 Author URI: http://www.groundhogg.io
 Text Domain: groundhogg
@@ -16,7 +16,7 @@ if ( ! class_exists( 'Groundhogg' ) ) :
     final class Groundhogg
     {
 
-        public $version = '1.0.9';
+        public $version = '1.0.9.1';
 
         /**
          * @var $instance Groundhogg instance
