9f6b0f751b66f48295c0cd382ca18160c8987c10
tobeyadr <gO0UsDYxRwhz>
2019-02-13 10:44:04 -0500

    update version number

--- groundhogg.php
+++ groundhogg.php
@@ -3,7 +3,7 @@
 Plugin Name: Groundhogg
 Plugin URI: https://wordpress.org/plugins/groundhogg/
 Description: CRM and marketing automation for WordPress
-Version: 1.0.23.1
+Version: 1.0.24
 Author: Groundhogg Inc.
 Author URI: http://www.groundhogg.io
 Text Domain: groundhogg
