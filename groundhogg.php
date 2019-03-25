42eee703cddabe6581e00f135baf618d3927e41f
tobeyadr <gO0UsDYxRwhz>
2019-03-25 09:16:58 -0400

    send plain text emails in HTML content type with the_content filter.

--- groundhogg.php
+++ groundhogg.php
@@ -3,7 +3,7 @@
 Plugin Name: Groundhogg
 Plugin URI: https://wordpress.org/plugins/groundhogg/
 Description: CRM and marketing automation for WordPress
-Version: 1.2.10.1
+Version: 1.2.10.2
 Author: Groundhogg Inc.
 Author URI: http://www.groundhogg.io
 Text Domain: groundhogg
@@ -16,7 +16,7 @@ if ( ! class_exists( 'Groundhogg' ) ) :
     final class Groundhogg
     {
 
-        public $version = '1.2.10.1';
+        public $version = '1.2.10.2';
 
         /**
          * @var $instance Groundhogg instance
