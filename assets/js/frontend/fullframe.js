ab576839fac400b43d8ec5bc9ec3e7ad218af427
Dhrumit07 <dhrumit07@gmail.com>
2019-07-02 16:45:24 -0400

    make changes in frame javascript file

--- assets/js/frontend/fullframe.js
+++ assets/js/frontend/fullframe.js
@@ -1,31 +1,73 @@
-jQuery(function($){
+(function($){
 
-    var iFrames = $('iframe');
+    function initAllFrames() {
+        var iFrames = $('iframe');
 
-    var offset = 0;
+        var offset = 0;
 
-    function iResize() {
-        for (var i = 0, j = iFrames.length; i < j; i++) {
-            iFrames[i].height($(iFrames[i].contentWindow.document).height());
+        function iResize() {
+            for (var i = 0, j = iFrames.length; i < j; i++) {
+                iFrames[i].height($(iFrames[i].contentWindow.document).height());
+            }
+        }
+
+        if ($.browser.safari || $.browser.opera) {
+
+            iFrames.load(function(){
+                setTimeout(iResize, 0);
+            });
+
+            for (var i = 0, j = iFrames.length; i < j; i++) {
+                var iSource = iFrames[i].src;
+                iFrames[i].src = '';
+                iFrames[i].src = iSource;
+            }
+
+        } else {
+            iFrames.load(function() {
+                $(this).height( $(this.contentWindow.document ).height() );
+            });
         }
     }
 
-    if ($.browser.safari || $.browser.opera) {
-
-        iFrames.load(function(){
-            setTimeout(iResize, 0);
-        });
-
-        for (var i = 0, j = iFrames.length; i < j; i++) {
-            var iSource = iFrames[i].src;
-            iFrames[i].src = '';
-            iFrames[i].src = iSource;
+    function addEvent( event, callback ){
+        if (!window.addEventListener) { // This listener will not be valid in < IE9
+            window.attachEvent("on" + event, callback);
+        } else { // For all other browsers other than < IE9
+            window.addEventListener( event, callback, false);
         }
-
-    } else {
-        iFrames.load(function() {
-            $(this).height( $(this.contentWindow.document ).height() );
-        });
     }
 
-});
+    function resizeAllFrames() {
+        var iFrames = $('iframe');
+        for (var i = 0; i < iFrames.length; i++ ){
+            var ifrm = iFrames[ i ];
+            var $ifrm = $( ifrm );
+            $ifrm.attr( 'id', 'frame-' + i );
+            var height = ifrm.contentWindow.postMessage( { action:'getFrameSize', id: $ifrm.attr( 'id') }, "*");
+        }
+    }
+
+    function receiveMessage(event) {
+        resizeFrame( event.data );
+    }
+
+    function resizeFrame( data ) {
+        if (data.height) {
+            var f = document.getElementById( data.id );
+            if (f) {
+                f.height = data.height;
+                f.width = data.width;
+                f.style.height = data.height;
+                f.style.width = data.width;
+            }
+        }
+    }
+
+    addEvent( 'message', receiveMessage );
+    addEvent( 'resize', resizeAllFrames );
+    addEvent( 'load', resizeAllFrames );
+
+    initAllFrames();
+
+})(jQuery);
