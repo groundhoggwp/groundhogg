2c8a7f0a186c98330e86b46d17e06b385a153d3a
Dhrumit07 <dhrumit07@gmail.com>
2019-10-10 14:58:35 -0400

    Merge branch 'master' of C:\Users\atty\PhpstormProjects\Groundhogg with conflicts.

--- admin/admin-menu.php
+++ admin/admin-menu.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Groundhogg\Admin;
 
 use Groundhogg\Admin\Broadcasts\Broadcasts_Page;
@@ -10,11 +11,11 @@ use Groundhogg\Admin\Events\Events_Page;
 use Groundhogg\Admin\Funnels\Funnels_Page;
 use Groundhogg\Admin\Guided_Setup\Guided_Setup;
 use Groundhogg\Admin\Settings\Settings_Page;
-use Groundhogg\Admin\SMS\SMS_Page;
-use Groundhogg\Admin\Superlinks\Superlinks_Page;
 use Groundhogg\Admin\Tags\Tags_Page;
 use Groundhogg\Admin\Tools\Tools_Page;
 use Groundhogg\Admin\Welcome\Welcome_Page;
+use function Groundhogg\is_option_enabled;
+use function Groundhogg\is_pro_features_active;
 use function Groundhogg\is_white_labeled;
 use function Groundhogg\isset_not_empty;
 use function Groundhogg\white_labeled_name;
@@ -44,24 +45,27 @@ class Admin_Menu
     /**
      * Setup the base DBs for the plugin
      */
-    public function init_admin(){
+    public function init_admin()
+    {
 
-        $this->welcome  = new Welcome_Page();
+        $this->welcome = new Welcome_Page();
         $this->contacts = new Contacts_Page();
-        $this->tags     = new Tags_Page();
-        $this->emails   = new Emails_Page();
-        $this->sms      = new SMS_Page();
+        $this->tags = new Tags_Page();
+
+        if ( ! is_pro_features_active() || ! is_option_enabled( 'gh_use_advanced_email_editor' ) ) {
+            $this->emails = new Emails_Page();
+        }
+
         $this->broadcasts = new Broadcasts_Page();
-        $this->funnels  = new Funnels_Page();
-        $this->superlinks = new Superlinks_Page();
-        $this->events   = new Events_Page();
-        $this->tools    = new Tools_Page();
+        $this->funnels = new Funnels_Page();
+        $this->events = new Events_Page();
+        $this->tools = new Tools_Page();
         $this->settings = new Settings_Page();
         $this->bulk_jobs = new Bulk_Job_Page();
 
         $this->dashboard = new Dashboard_Widgets();
 
-        if (!is_white_labeled()) {
+        if ( !is_white_labeled() ) {
             $this->guided_setup = new Guided_Setup();
         }
 
@@ -74,7 +78,8 @@ class Admin_Menu
      * @param $key string
      * @return Admin_Page|Funnels_Page|Contacts_Page
      */
-    public function get_page( $key ){
+    public function get_page( $key )
+    {
         return $this->$key;
     }
 
@@ -86,7 +91,7 @@ class Admin_Menu
      */
     public function __get( $key )
     {
-        if ( isset_not_empty( $this->pages, $key ) ){
+        if ( isset_not_empty( $this->pages, $key ) ) {
             return $this->pages[ $key ];
         }
 
