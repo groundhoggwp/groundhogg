a8e612aa435056227d21e26dcd22f6f2eaf5f065
tobeyadr <gO0UsDYxRwhz>
2019-10-15 09:58:54 -0400

    disable safe mode notice has link to disable safe mode automatically instead of redirect to the tools page.

--- includes/classes/email.php
+++ includes/classes/email.php
@@ -733,6 +733,7 @@ class Email extends Base_Object_With_Meta
      */
     public function send( $contact_id_or_email, $event = 0 )
     {
+        $this->clear_errors();
 
         if ( $this->is_draft() && ! $this->is_testing() ){
             return new WP_Error('email_not_ready', sprintf( __( 'Emails cannot be sent in %s mode.', 'groundhogg' ), $this->get_status() ) );
