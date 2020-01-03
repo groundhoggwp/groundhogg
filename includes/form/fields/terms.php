aa960c07e0b65468395ef3bcd941d7bccc31eb0d
tobeyadr <gO0UsDYxRwhz>
2020-01-03 14:58:50 -0500

    add view terms link to terms field

--- includes/form/fields/terms.php
+++ includes/form/fields/terms.php
@@ -1,6 +1,8 @@
 <?php
 namespace Groundhogg\Form\Fields;
 
+use function Groundhogg\html;
+
 /**
  * Created by PhpStorm.
  * User: adria
@@ -22,6 +24,36 @@ class Terms extends Checkbox
             'title'         => _x( 'Please agree to the terms of service.', 'form_default', 'groundhogg' ),
             'required'      => true,
         ];
+    }
+
+    public function render()
+    {
+        $terms_link = get_option( 'gh_terms' );
+
+        $label = $this->get_label();
+
+        if ( $terms_link ){
+            $terms_link = html()->e( 'a', [
+                'href' => $terms_link,
+                'class' => 'gh-terms-link',
+                'target' => '_blank'
+            ], __( 'view terms', 'groundhogg' ) );
+
+            $label = sprintf( "%s (%s)", $label, $terms_link );
+        }
+
+        $atts = [
+            'label' => $label,
+            'name'  => $this->get_name(),
+            'id'    => $this->get_id(),
+            'class' => $this->get_classes() . ' gh-checkbox',
+            'value' => $this->get_value(),
+            'title' => $this->get_title(),
+            'required' => $this->is_required(),
+            'checked' => $this->is_checked()
+        ];
+
+        return html()->checkbox( $atts );
     }
 
     /**
