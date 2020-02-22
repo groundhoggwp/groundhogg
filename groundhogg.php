4fe6fb72cee648036a0d878917dddc69ea9d0495
tobeyadr <adrian@trainingbusinesspros.com>
2020-02-22 08:56:01 -0500

    bump version #

--- groundhogg.php
+++ groundhogg.php
@@ -3,7 +3,7 @@
  * Plugin Name: Groundhogg
  * Plugin URI:  https://www.groundhogg.io/?utm_source=wp-plugins&utm_campaign=plugin-uri&utm_medium=wp-dash
  * Description: CRM and marketing automation for WordPress
- * Version: 2.1.13.4
+ * Version: 2.1.13.5
  * Author: Groundhogg Inc.
  * Author URI: https://www.groundhogg.io/?utm_source=wp-plugins&utm_campaign=author-uri&utm_medium=wp-dash
  * Text Domain: groundhogg
