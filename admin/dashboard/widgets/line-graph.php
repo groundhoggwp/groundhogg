de8732df8dba3c1bd7226e18eff5b9d86a30f6ab
Dhrumit07 <dhrumit07@gmail.com>
2020-02-27 12:58:39 -0500

    added proper line charts

--- admin/dashboard/widgets/line-graph.php
+++ admin/dashboard/widgets/line-graph.php
@@ -56,7 +56,6 @@ abstract class Line_Graph extends Dashboard_Widget {
 		if ( ! $is_empty ):
 
 
-
 			foreach ( $data as $d ) {
 				$label = 'a';
 			}
@@ -74,134 +73,129 @@ abstract class Line_Graph extends Dashboard_Widget {
 			?>
             <div class="report">
                 <script type="text/javascript">
-                    jQuery(function ($) {
-
-                        /* DATA OPERATION */
-                        var dataset = <?php echo $data; ?>;
-                        console.log(dataset);
-                        var options = {
-                            series: {
-                                lines: {show: true},
-                                points: {
-                                    radius: 3,
-                                    show: true
-                                }
-                            },
-                            grid: {
-                                hoverable: true
-                            },
-                            xaxis: {
-                                mode: "<?php echo $this->get_mode(); ?>",
-                                localTimezone: true
-                            }
-                        };
-
-                        /* TOOL TIP */
-                        var previousPoint = null, previousLabel = null;
-
-						<?php if ( $this->get_mode() === 'time' ): ?>
-
-						<?php $offset = intval( get_option( 'gmt_offset' ) ) * HOUR_IN_SECONDS * 1000; ?>
-                        $.fn.UseTooltipTime = function () {
-                            $(this).bind("plothover", function (event, pos, item) {
-                                if (item) {
-                                    if ((previousLabel != item.series.label) || (previousPoint != item.dataIndex)) {
-                                        previousPoint = item.dataIndex;
-                                        previousLabel = item.series.label;
-                                        $("#tooltip").remove();
-                                        var x = item.datapoint[0];
-                                        var y = item.datapoint[1];
-                                        var color = item.series.color;
-                                        var date = new Date(x - <?php echo $offset; ?> );
-                                        showTooltip(item.pageX, item.pageY, color, "<strong>" + item.series.label + "</strong>: " + y + "<br/>" + date.toDateString());
-                                    }
-                                } else {
-                                    $("#tooltip").remove();
-                                    previousPoint = null;
-                                }
-                            });
-                        };
-
-						<?php else: ?>
-
-                        $.fn.UseTooltip = function () {
-                            $(this).bind("plothover", function (event, pos, item) {
-                                if (item) {
-                                    if ((previousLabel != item.series.label) || (previousPoint != item.dataIndex)) {
-                                        previousPoint = item.dataIndex;
-                                        previousLabel = item.series.label;
-                                        $("#tooltip").remove();
-                                        var x = item.datapoint[0];
-                                        var y = item.datapoint[1];
-                                        var color = item.series.color;
-                                        showTooltip(item.pageX, item.pageY, color, "<strong>" + item.series.label + "</strong>: " + y + "<br/>");
-                                    }
-                                } else {
-                                    $("#tooltip").remove();
-                                    previousPoint = null;
-                                }
-                            });
-                        };
-
-						<?php endif; ?>
-
-                        function showTooltip(x, y, color, contents) {
-                            $('<div id="tooltip">' + contents + '</div>').css({
-                                position: 'absolute',
-                                display: 'none',
-                                top: y - 40,
-                                left: x - 120,
-                                border: '2px solid ' + color,
-                                padding: '3px',
-                                'font-size': '9px',
-                                'border-radius': '5px',
-                                'background-color': '#fff',
-                                'font-family': 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
-                                'z-index': '99999',
-                                opacity: 0.9
-                            }).appendTo("body").fadeIn(200);
-                        }
-
-                        /* PLOT CHART */
-
-                        function draw() {
-                            if ($("#graph-<?php echo $this->get_id(); ?>").width() > 0) {
-                                $.plot($("#graph-<?php echo $this->get_id(); ?>"), dataset, options);
-
-								<?php if ( $this->get_mode() === 'time' ): ?>
-                                $("#graph-<?php echo $this->get_id(); ?>").UseTooltipTime();
-								<?php else: ?>
-                                $("#graph-<?php echo $this->get_id(); ?>").UseTooltip();
-								<?php endif; ?>
-                            }
-                        }
-
-                        setTimeout(draw, 500);
-
-                    });
+                    //jQuery(function ($) {
+                    //
+                    //    /* DATA OPERATION */
+                    //    var dataset = <?php //echo $data; ?>//;
+                    //    console.log(dataset);
+                    //    var options = {
+                    //        series: {
+                    //            lines: {show: true},
+                    //            points: {
+                    //                radius: 3,
+                    //                show: true
+                    //            }
+                    //        },
+                    //        grid: {
+                    //            hoverable: true
+                    //        },
+                    //        xaxis: {
+                    //            mode: "<?php //echo $this->get_mode(); ?>//",
+                    //            localTimezone: true
+                    //        }
+                    //    };
+                    //
+                    //    /* TOOL TIP */
+                    //    var previousPoint = null, previousLabel = null;
+                    //
+                    //	<?php //if ( $this->get_mode() === 'time' ): ?>
+                    <!---->
+                    <!--	--><?php //$offset = intval( get_option( 'gmt_offset' ) ) * HOUR_IN_SECONDS * 1000; ?>
+                    //    $.fn.UseTooltipTime = function () {
+                    //        $(this).bind("plothover", function (event, pos, item) {
+                    //            if (item) {
+                    //                if ((previousLabel != item.series.label) || (previousPoint != item.dataIndex)) {
+                    //                    previousPoint = item.dataIndex;
+                    //                    previousLabel = item.series.label;
+                    //                    $("#tooltip").remove();
+                    //                    var x = item.datapoint[0];
+                    //                    var y = item.datapoint[1];
+                    //                    var color = item.series.color;
+                    //                    var date = new Date(x - <?php //echo $offset; ?>// );
+                    //                    showTooltip(item.pageX, item.pageY, color, "<strong>" + item.series.label + "</strong>: " + y + "<br/>" + date.toDateString());
+                    //                }
+                    //            } else {
+                    //                $("#tooltip").remove();
+                    //                previousPoint = null;
+                    //            }
+                    //        });
+                    //    };
+                    //
+                    //	<?php //else: ?>
+                    //
+                    //    $.fn.UseTooltip = function () {
+                    //        $(this).bind("plothover", function (event, pos, item) {
+                    //            if (item) {
+                    //                if ((previousLabel != item.series.label) || (previousPoint != item.dataIndex)) {
+                    //                    previousPoint = item.dataIndex;
+                    //                    previousLabel = item.series.label;
+                    //                    $("#tooltip").remove();
+                    //                    var x = item.datapoint[0];
+                    //                    var y = item.datapoint[1];
+                    //                    var color = item.series.color;
+                    //                    showTooltip(item.pageX, item.pageY, color, "<strong>" + item.series.label + "</strong>: " + y + "<br/>");
+                    //                }
+                    //            } else {
+                    //                $("#tooltip").remove();
+                    //                previousPoint = null;
+                    //            }
+                    //        });
+                    //    };
+                    //
+                    //	<?php //endif; ?>
+                    //
+                    //    function showTooltip(x, y, color, contents) {
+                    //        $('<div id="tooltip">' + contents + '</div>').css({
+                    //            position: 'absolute',
+                    //            display: 'none',
+                    //            top: y - 40,
+                    //            left: x - 120,
+                    //            border: '2px solid ' + color,
+                    //            padding: '3px',
+                    //            'font-size': '9px',
+                    //            'border-radius': '5px',
+                    //            'background-color': '#fff',
+                    //            'font-family': 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
+                    //            'z-index': '99999',
+                    //            opacity: 0.9
+                    //        }).appendTo("body").fadeIn(200);
+                    //    }
+                    //
+                    //    /* PLOT CHART */
+                    //
+                    //    function draw() {
+                    //        if ($("#graph-<?php //echo $this->get_id(); ?>//").width() > 0) {
+                    //            $.plot($("#graph-<?php //echo $this->get_id(); ?>//"), dataset, options);
+                    //
+                    //			<?php //if ( $this->get_mode() === 'time' ): ?>
+                    //            $("#graph-<?php //echo $this->get_id(); ?>//").UseTooltipTime();
+                    //			<?php //else: ?>
+                    //            $("#graph-<?php //echo $this->get_id(); ?>//").UseTooltip();
+                    //			<?php //endif; ?>
+                    //        }
+                    //    }
+                    //
+                    //    setTimeout(draw, 500);
+                    //
+                    //});
                 </script>
-                <div id="graph-<?php echo $this->get_id(); ?>" style="width:auto;height: 250px;"></div>
+                <!--                <div id="graph---><?php //echo $this->get_id();
+				?><!--" style="width:auto;height: 250px;"></div>-->
 
-
+                <script src="http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min.js"></script>
                 <canvas id="chart-<?php echo $this->get_id(); ?>" style="width:auto;height: 250px;"></canvas>
                 <script>
 
-                    console.log(<?php echo $this->get_id(); ?>);
-                    console.log(<?php echo $data_chart ;?> );
+                    //console.log(<?php //echo $this->get_id(); ?>//);
+                    //console.log(<?php //echo $data_chart;?>// );
+
 
                     var ctx = document.getElementById('<?php echo 'chart-' . $this->get_id();  ?>').getContext('2d');
                     var myChart = new Chart(ctx, {
                         type: 'line',
                         data: <?php echo $data_chart; ?>,
-                        options: {
-                            scales: {
-                                yAxes: [{
-                                    ticks: {
-                                        beginAtZero: true
-                                    }
-                                }]
-                            }
-                        }
+                        options: <?php echo $this->get_chart_options() ?>,
+
                     });
                 </script>
 
@@ -255,60 +249,95 @@ abstract class Line_Graph extends Dashboard_Widget {
 		return $data;
 	}
 
+
 	public function get_data_chart() {
-		$data = [];
 
-		foreach ( $this->get_report_ids() as $report_id ) {
+		$data = $this->get_data();
 
-			$report = Plugin::$instance->reporting->get_report( $report_id );
-
-			if ( $report && method_exists( $this, 'normalize_' . $report_id ) ) {
-				$data[] = [
-					'label' => $report->get_name(),
-					'data'  => call_user_func( [ $this, 'normalize_' . $report_id ], $report->get_data() )
-				];
-			}
-
-		}
-
-		$mydata = [];
-
-		$label_chart = [];
+		$mydata      = [];
 		$label_chart = [];
 		foreach ( $data[ 0 ][ 'data' ] as $item ) {
 			$label_chart[] = $item[ 0 ];
 		}
 
+		$i = 1;
 		foreach ( $data as $d ) {
 
 			$values = [];
 
 			foreach ( $d[ 'data' ] as $v ) {
 				$values[] = [
-                    'x' => $v[0],
-                    'y' => $v[ 1 ]
-                ];
+					'x' => $v[ 0 ],
+					'y' => $v[ 1 ]
+				];
 
 			}
 
 
 			$mydata[] = [
-				'label' => $d[ 'label' ],
-				'data'  => $values
+				'label'           => $d[ 'label' ],
+				"backgroundColor" => $this->get_bg_color( $i ),
+				"borderColor"     => $this->get_border_color( $i ),
+				'data'            => $values
 			];
 
+			$i ++;
 		}
 
-
 		return [
-            'labels' => $label_chart,
-            'datasets'=>$mydata
-        ];
+			'labels'   => $label_chart,
+			'datasets' => $mydata
+		];
 
+	}
+
+
+	public function get_bg_color( $i ) {
+		$items = [
+			"rgba(232, 116, 59 , 0.5 )",
+			"rgba(88, 153, 218 , 0.5 )",
+			"rgba(25, 169, 121 , 0.5 )",
+			"rgba(237, 74, 123 , 0.5 )",
+			"rgba(19, 164, 180 , 0.5 )"
+		];
+
+		return $items [ intval( ( $i % 5 ) - 1 ) ];
+	}
+
+	public function get_border_color( $i ) {
+		$items = [
+			"rgba(232, 116, 59 , 1)",
+			"rgba(88, 153, 218 , 1)",
+			"rgba(25, 169, 121 , 1)",
+			"rgba(237, 74, 123 , 1)",
+			"rgba(19, 164, 180 , 1)"
+		];
+
+		return $items [ intval( ( $i % 5 ) - 1 ) ];
 	}
 
 
 	public function get_chart_data() {
 		return $this->get_data();
 	}
+
+	public function get_chart_options() {
+
+
+		return "{
+		    responsive: true,         
+            scales: {                    
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    },
+                    scaleLabel: {
+						display:     true,
+                        labelString: 'value'
+                    }
+                }]
+            }
+        }";
+	}
+
 }
--- admin/dashboard/widgets/time-graph.php
+++ admin/dashboard/widgets/time-graph.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Groundhogg\Admin\Dashboard\Widgets;
 
 use function Groundhogg\isset_not_empty;
@@ -10,135 +11,156 @@ use Groundhogg\Plugin;
  * Date: 11/27/2018
  * Time: 9:13 AM
  */
+abstract class Time_Graph extends Line_Graph {
+	/**
+	 * @return string
+	 */
+	public function get_mode() {
+		return 'time';
+	}
 
-abstract class Time_Graph extends Line_Graph
-{
-    /**
-     * @return string
-     */
-    public function get_mode(){
-        return 'time';
-    }
+	/**
+	 * @param $datum
+	 *
+	 * @return int
+	 */
+	abstract public function get_time_from_datum( $datum );
 
-    /**
-     * @param $datum
-     * @return int
-     */
-    abstract public function get_time_from_datum( $datum );
+	/**
+	 * Get the time slots for the given time range...
+	 *
+	 * @return array
+	 */
+	public function get_date_points() {
+		$points = Plugin::$instance->reporting->get_points();
+		$start  = Plugin::$instance->reporting->get_start_time();
+		$diff   = Plugin::$instance->reporting->get_difference();
 
-    /**
-     * Get the time slots for the given time range...
-     *
-     * @return array
-     */
-    public function get_date_points()
-    {
-        $points = Plugin::$instance->reporting->get_points();
-        $start = Plugin::$instance->reporting->get_start_time();
-        $diff = Plugin::$instance->reporting->get_difference();
+		$date_points = [];
 
-        $date_points = [];
+		for ( $i = 0; $i < $points; $i ++ ) {
+			$start                 = Plugin::$instance->utils->date_time->round_to( $start, $diff );
+			$date_points[ $start ] = [ $start * 1000, 0, date( 'Y-m-d H:i:s', $start ) ];
+			$start                 += $diff;
+		}
 
-        for ( $i = 0;$i<$points;$i++){
-            $start = Plugin::$instance->utils->date_time->round_to( $start, $diff );
-            $date_points[ $start ] = [ $start * 1000, 0, date( 'Y-m-d H:i:s', $start ) ];
-            $start+=$diff;
-        }
+		return $date_points;
+	}
 
-        return $date_points;
-    }
+	/**
+	 * Group the given data into their respective time slots...
+	 *
+	 * @param $data
+	 *
+	 * @return array
+	 */
+	public function group_by_time( $data ) {
 
-    /**
-     * Group the given data into their respective time slots...
-     *
-     * @param $data
-     * @return array
-     */
-    public function group_by_time( $data ){
+		$times = $this->get_date_points();
 
-        $times = $this->get_date_points();
+		foreach ( $data as $datum ) {
+			$date_point = Plugin::$instance->utils->date_time->round_to(
+				$this->get_time_from_datum( $datum ),
+				Plugin::$instance->reporting->get_difference(),
+				false
+			);
 
-        foreach ( $data as $datum ){
-            $date_point = Plugin::$instance->utils->date_time->round_to(
-                    $this->get_time_from_datum( $datum ),
-                    Plugin::$instance->reporting->get_difference(),
-                false
-            );
+			if ( isset_not_empty( $times, $date_point ) ) {
+				$times[ $date_point ][ 1 ] ++;
+			}
+		}
 
-            if ( isset_not_empty( $times, $date_point ) ){
-                $times[ $date_point ][ 1 ]++;
-            }
-        }
+		return array_values( $times );
+	}
 
-        return array_values( $times );
-    }
+	/**
+	 * @return array
+	 */
+	public function get_data() {
+		$data = [];
 
-    /**
-     * @return array
-     */
-    public function get_data()
-    {
-        $data = [];
+		foreach ( $this->get_report_ids() as $report_id ) {
 
-        foreach ( $this->get_report_ids() as $report_id ){
+			$report = Plugin::$instance->reporting->get_report( $report_id );
 
-            $report = Plugin::$instance->reporting->get_report( $report_id );
+			if ( $report ) {
+				$data[] = [
+					'label' => $report->get_name(),
+					'data'  => $this->group_by_time( $report->get_data() )
+				];
+			}
+		}
 
-            if ( $report ){
-                $data[] = [
-                    'label' => $report->get_name(),
-                    'data'  => $this->group_by_time( $report->get_data() )
-                ];
-            }
-        }
+		$this->dataset = $data;
 
-        $this->dataset = $data;
-
-        return $data;
-    }
-
-
+		return $data;
+	}
 
 
 	public function get_data_chart() {
 
-    	$data = $this->get_data();
+		$data = $this->get_data();
 
 		$mydata = [];
-		$label_chart = [];
 
-
-		foreach ( $data[ 0 ][ 'data' ] as $item ) {
-			$label_chart[] = date( 'Y-m-d' , $item[0]/1000 );
-		}
-
+		$i = 1;
 		foreach ( $data as $d ) {
 
 			$values = [];
 
 			foreach ( $d[ 'data' ] as $v ) {
 				$values[] = [
-					't' => date( 'Y-m-d' , $v[0]/1000 ),
+					't' => $v[ 2 ],
 					'y' => $v[ 1 ]
 				];
 
 			}
 
-
 			$mydata[] = [
-				'label' => $d[ 'label' ],
-				'data'  => $values
+				'label'           => $d[ 'label' ],
+				"backgroundColor" => $this->get_bg_color( $i ),
+				"borderColor"     => $this->get_border_color( $i ),
+				'data'            => $values,
+				"fill"            => false
 			];
+
+			$i ++;
 
 		}
 
 		return [
-			'labels' => $label_chart,
-			'datasets'=>$mydata
+			'datasets' => $mydata
 		];
 
 	}
 
+
+	public function get_chart_options() {
+
+		return '{
+			responsive: true,         
+            scales:     {
+				xAxes: [{
+					type:       "time",
+                    time:       {
+						parser: "YYY-MM-DD HH:mm:ss",
+                        tooltipFormat: "l HH:mm"
+                    },
+                    scaleLabel: {
+						display:     true,
+                        labelString: "Date"
+                    }
+                }],
+                yAxes: [{
+					scaleLabel: {
+						display:     true,
+                        labelString: "value"
+                    }
+				}]
+            }
+        }';
+
+	}
 
 
 }
