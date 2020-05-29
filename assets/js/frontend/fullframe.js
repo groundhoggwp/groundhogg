72ed1e5abc97d93ba174f06c7c8d9f08971c5b78
Dhrumit <dhrumit07@gmail.com>
2020-05-29 14:00:57 -0400

    Updated the full frame JS which I believe fix credic's issue

--- assets/js/frontend/fullframe.js
+++ assets/js/frontend/fullframe.js
@@ -1,4 +1,4 @@
-(function($){
+(function ($) {
 
     function initAllFrames() {
         var iFrames = $('iframe');
@@ -7,13 +7,18 @@
 
         function iResize() {
             for (var i = 0, j = iFrames.length; i < j; i++) {
-                iFrames[i].height($(iFrames[i].contentWindow.document).height());
+                // console.log($(iFrames[i].contentWindow.document.body.offsetHeight));
+                // iFrames[i].style.height = $(iFrames[i].contentWindow.document.body.offsetHeight) ;
+
+                //replaced function with assignment in case it fix it..
+                iFrames[i].style.height = $(iFrames[i].contentWindow.document.body.scrollHeight)+"px";
+                // iFrames[i].height($(iFrames[i].contentWindow.document).height());
             }
         }
 
         if ($.browser.safari || $.browser.opera) {
 
-            iFrames.load(function(){
+            iFrames.load(function () {
                 setTimeout(iResize, 0);
             });
 
@@ -24,50 +29,50 @@
             }
 
         } else {
-            iFrames.load(function() {
-                $(this).height( $(this.contentWindow.document ).height() );
+            iFrames.load(function () {
+                $(this).height($(this.contentWindow.document).height());
             });
         }
 
-        console.log( iFrames );
+        // console.log( iFrames );
     }
 
-    function addEvent( event, callback ){
+    function addEvent(event, callback) {
         if (!window.addEventListener) { // This listener will not be valid in < IE9
             window.attachEvent("on" + event, callback);
         } else { // For all other browsers other than < IE9
-            window.addEventListener( event, callback, false);
+            window.addEventListener(event, callback, false);
         }
     }
 
     function resizeAllFrames() {
         var iFrames = $('iframe');
-        for (var i = 0; i < iFrames.length; i++ ){
-            var ifrm = iFrames[ i ];
-            var $ifrm = $( ifrm );
-            $ifrm.attr( 'id', 'frame-' + ( i + 1 ) );
-            var height = ifrm.contentWindow.postMessage( { action:'getFrameSize', id: $ifrm.attr( 'id') }, "*");
+        for (var i = 0; i < iFrames.length; i++) {
+            var ifrm = iFrames[i];
+            var $ifrm = $(ifrm);
+            $ifrm.attr('id', 'frame-' + (i + 1));
+            var height = ifrm.contentWindow.postMessage({action: 'getFrameSize', id: $ifrm.attr('id')}, "*");
         }
     }
 
     function receiveMessage(event) {
         // console.log( event.data );
-        resizeFrame( event.data );
+        resizeFrame(event.data);
     }
 
-    function resizeFrame( data ) {
+    function resizeFrame(data) {
         if (data.height) {
-            var f = $( '#' + data.id );
+            var f = $('#' + data.id);
             if (f) {
-                f.height( data.height );
-                f.width( data.width );
+                f.height(data.height);
+                f.width(data.width);
             }
         }
     }
 
-    addEvent( 'message', receiveMessage );
-    addEvent( 'resize', resizeAllFrames );
-    addEvent( 'load', resizeAllFrames );
+    addEvent('message', receiveMessage);
+    addEvent('resize', resizeAllFrames);
+    addEvent('load', resizeAllFrames);
 
     $(initAllFrames);
 
