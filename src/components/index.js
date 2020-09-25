77dbbd2044b99301a35a30aeaee4fd9dad74b2c8
Justin Sainton <justinsainton@gmail.com>
2020-09-24 18:07:23 -0700

    Export PageLayout and UI components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { PageLayout } from './layout';
+export { default as Animations } from './core-ui/animations';
+export { default as BottomBar } from './layout/bottom-bar';
+export { default as BulkJob } from './core-ui/bulk-job';
+export { default as Buttons } from './core-ui/buttons';
+export { default as ListTable } from './core-ui/list-table';
+export { default as TagPicker } from './core-ui/tag-picker';
+export { default as VideoModal } from './core-ui/video-modal';
