dd48e46ded3c2c26e8bdf714f54b0984d24d2aed
Justin Sainton <justinsainton@gmail.com>
2020-09-24 18:09:45 -0700

    Add light controller layer. * Uses Suspense component with Fallback for rendering * getPages() constant should be where core pages are added * Lazy importing for webpack chunking may be useful, may be not. Possibly worth eliminating

---
+++ src/components/layout/controller.js
@@ -0,0 +1,59 @@
+/**
+ * External dependencies
+ */
+import { Component, Suspense, lazy } from '@wordpress/element';
+import { parse } from 'qs';
+import { applyFilters } from '@wordpress/hooks';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { Spinner } from '../core-ui';
+
+const Dashboard = lazy( () =>
+	import( /* webpackChunkName: "dashboard" */ 'dashboard' )
+);
+
+export const PAGES_FILTER = 'groundhogg_navigation';
+
+export const getPages = () => {
+	const pages = [];
+
+	/** @TODO: parse/hydrate PHP-registered nav items for app navigation */
+	pages.push( {
+		container: Dashboard,
+		path: '/dashboard',
+	} );
+
+	return applyFilters( PAGES_FILTER, pages );
+};
+
+export class Controller extends Component {
+
+	getQuery( searchString ) {
+		if ( ! searchString ) {
+			return {};
+		}
+
+		const search = searchString.substring( 1 );
+		return parse( search );
+	}
+
+	render() {
+		const { page, match, location } = this.props;
+		const { url, params } = match;
+		const query = this.getQuery( location.search );
+
+		return (
+			<Suspense fallback={ <Spinner /> }>
+				<page.container
+					params={ params }
+					path={ url }
+					pathMatch={ page.path }
+					query={ query }
+				/>
+			</Suspense>
+		);
+	}
+}
