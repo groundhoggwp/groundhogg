bfe185ca2725a813c755a7d68013654db2d6326d
Justin Sainton <justinsainton@gmail.com>
2020-09-24 18:11:43 -0700

    Stubbing out Notices and Styles for layout component

---
+++ src/components/layout/notices.js
@@ -0,0 +1,17 @@
+/**
+ * External dependencies
+ */
+import { Component } from '@wordpress/element';
+
+class Notices extends Component {
+	render() {
+		return (
+			<div
+				id="groundhogg-layout__notice-list"
+				className="groundhogg-layout__notice-list"
+			/>
+		);
+	}
+}
+
+export default Notices;
