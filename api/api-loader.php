273f49829d92bf2eb51a3e35a16436d01ceb2513
tobeyadr <adrian@trainingbusinesspros.com>
2020-10-03 15:51:38 -0400

    made authorization possible via API key for v4

--- api/api-loader.php
+++ api/api-loader.php
@@ -4,6 +4,7 @@ namespace Groundhogg\Api;
 
 use Groundhogg\Api\V3\API_V3;
 use Groundhogg\Api\V4\API_V4_HANDLER;
+use function Groundhogg\get_array_var;
 
 /**
  * Created by PhpStorm.
@@ -19,16 +20,78 @@ class Api_Loader {
 	public $v3;
 
 	/**
+	 * @var API_V4_HANDLER
+	 */
+	public $v4;
+
+	/**
 	 * WPGH_API_LOADER constructor.
 	 */
 	public function __construct() {
 
 		add_action( 'rest_api_init', [ $this, 'load_api' ] );
+		add_action( 'rest_api_init', [ $this, 'handle_api_key_usage' ] );
 	}
 
 	public function load_api() {
 		$this->v3 = new API_V3();
 		$this->v4 = new API_V4_HANDLER();
+	}
+
+
+	/**
+	 * @param $rest_server \WP_REST_Server
+	 *
+	 * @return bool|void
+	 */
+	public function handle_api_key_usage( $rest_server ) {
+
+		/* If the current user is logged in then we can bypass the key authentication */
+		if ( is_user_logged_in() ) {
+			return;
+		}
+
+		$headers = $rest_server->get_headers( $_SERVER );
+
+		$token = get_array_var( $headers, 'GH_TOKEN' );
+		$key   = get_array_var( $headers, 'GH_PUBLIC_KEY' );
+
+		if ( ! $token || ! $key ) {
+			return;
+		}
+
+		//validate user
+		global $wpdb;
+
+		$user_id = $wpdb->get_var( $wpdb->prepare( "SELECT user_id FROM $wpdb->usermeta WHERE meta_key = 'wpgh_user_public_key' AND meta_value = %s LIMIT 1", $key ) );
+
+		if ( ! $user_id ) {
+			return;
+		}
+
+		$secret = get_user_meta( $user_id, 'wpgh_user_secret_key', true );
+
+		if ( ! self::check_keys( $secret, $key, $token ) ) {
+			return;
+		}
+
+		/**
+		 * Set the current user for the request
+		 */
+		wp_set_current_user( $user_id );
+	}
+
+	/**
+	 * Check the keys provided.
+	 *
+	 * @param $secret
+	 * @param $public
+	 * @param $token
+	 *
+	 * @return bool
+	 */
+	public static function check_keys( $secret, $public, $token ) {
+		return hash_equals( md5( $secret . $public ), $token );
 	}
 
 }
