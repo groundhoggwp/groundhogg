0d9a473109e1ee4a77c869f3f3a6a91281d9efe9
Justin Sainton <justinsainton@gmail.com>
2020-10-05 16:24:55 -0700

    Remove build from repo

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-polyfill', 'wp-url'), 'version' => '34035d3f53c784a4edbcd548147ab57c');
