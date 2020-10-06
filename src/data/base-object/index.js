85489126ed3aec8c482529098edc294f23c7b715
Justin Sainton <justinsainton@gmail.com>
2020-10-06 12:25:39 -0700

    Update actions and support optional extension of store components

--- src/data/base-object/index.js
+++ src/data/base-object/index.js
@@ -3,31 +3,32 @@
  */
 
 import { registerStore } from '@wordpress/data';
+import { assign } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import * as selectors from './selectors';
-import * as actions from './actions';
-import controls from '../controls';
-import reducer from './reducer';
-import * as resolvers from './resolvers';
+import * as baseSelectors from './selectors';
+import * as baseActions from './actions';
+import * as baseControls from '../controls';
+import * as baseReducer from './reducer';
+import * as baseResolvers from './resolvers';
 import { NAMESPACE } from '../constants'
 
 export function registerBaseObjectStore ( endpoint, options ) {
 
-	resolvers.getEndpoint = ( endpoint = storeName ) => endpoint;
+	baseResolvers.getEndpoint = ( endpoint = storeName ) => endpoint;
 
-	const storeName = NAMESPACE + '/' + endpoint;
-	const store     = registerStore( storeName, {
-		reducer,
-		actions,
-		selectors,
-		controls,
-		resolvers
-	} );
+	const storeArgs = {
+		reducer      : options.reducer   ? assign( baseReducer  , options.reducer )   : baseReducer,
+		actions      : options.actions   ? assign( baseActions  , options.actions )   : baseActions,
+		selectors    : options.selectors ? assign( baseSelectors, options.selectors ) : baseSelectors,
+		controls     : options.controls  ? assign( baseControls , options.controls )  : baseControls,
+		resolvers    : options.resolvers ? assign( baseResolvers, options.resolvers ) : baseResolvers,
+		initialState : options.initialState || {},
+	}
 
-	return store;
+	return registerStore( NAMESPACE + '/' + endpoint, storeArgs );
 }
 
 export function getStoreName( ghEndpoint ) {
