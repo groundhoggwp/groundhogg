77843d55d228f1901029ca88a39716b365501d3f
Justin Sainton <justinsainton@gmail.com>
2020-10-06 14:50:48 -0700

    More work on optional store extension

--- src/data/base-object/index.js
+++ src/data/base-object/index.js
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-
 import { registerStore } from '@wordpress/data';
 import { assign } from 'lodash';
 
@@ -10,21 +9,24 @@ import { assign } from 'lodash';
  */
 import * as baseSelectors from './selectors';
 import * as baseActions from './actions';
-import * as baseControls from '../controls';
-import * as baseReducer from './reducer';
+import controls from '../controls';
+import reducer from './reducer';
 import * as baseResolvers from './resolvers';
 import { NAMESPACE } from '../constants'
 
-export function registerBaseObjectStore (endpoint) {
+export function registerBaseObjectStore (endpoint, options) {
+  const storeName = NAMESPACE + '/' + endpoint
 
-	baseResolvers.getEndpoint = ( endpoint = storeName ) => endpoint;
+  options = options || {}
+
+	baseResolvers.getEndpoint = ( endpoint = storeName ) => endpoint
   baseActions.getEndpoint   = (endpoint = storeName) => endpoint
 
 	const storeArgs = {
-		reducer      : options.reducer   ? assign( baseReducer  , options.reducer )   : baseReducer,
+		reducer      : options.reducer   ? assign( reducer  , options.reducer )   : reducer,
+		controls     : options.controls  ? assign( controls , options.controls )  : controls,
 		actions      : options.actions   ? assign( baseActions  , options.actions )   : baseActions,
 		selectors    : options.selectors ? assign( baseSelectors, options.selectors ) : baseSelectors,
-		controls     : options.controls  ? assign( baseControls , options.controls )  : baseControls,
 		resolvers    : options.resolvers ? assign( baseResolvers, options.resolvers ) : baseResolvers,
 		initialState : options.initialState || {},
 	}
