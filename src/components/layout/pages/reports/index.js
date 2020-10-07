083ff6ea751685cf30f99662ec8f7e819d04f3c2
Nathan Sachs <>
2020-10-07 14:38:30 -0400

    Initial Chart JS on a few cards

--- src/components/layout/pages/reports/index.js
+++ src/components/layout/pages/reports/index.js
@@ -3,42 +3,77 @@
  */
 import { Fragment, useState } from '@wordpress/element';
 import { useSelect, useDispatch } from '@wordpress/data';
+import { makeStyles } from '@material-ui/core/styles';
 import { __ } from '@wordpress/i18n';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import Spinner from '../../../core-ui/spinner';
 
 /**
  * Internal dependencies
  */
-import { TAGS_STORE_NAME } from '../../../../data';
+ import Chart from '../../../core-ui/chart';
+ import Spinner from '../../../core-ui/spinner';
+// import { TAGS_STORE_NAME } from '../../../../data';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+		marginBottom: theme.spacing(1),
+		textAlign: 'center'
+    // paddingTop: theme.spacing(4),
+    // paddingBottom: theme.spacing(4),
+  },
+
+	kpiTitle: {
+		fontSize: '24px',
+		fontStyle: 'bold'
+	},
+	kpiMetric: {
+		fontSize: '16px'
+	}
+}));
+
 
 export const Reports = ( props ) => {
-	const [ stateTagValue, setTagValue ] = useState( '' );
-
-	const { updateTags } = useDispatch( TAGS_STORE_NAME );
-
-	const { tags, isRequesting, isUpdating } = useSelect( ( select ) => {
-		const store = select( TAGS_STORE_NAME );
-		return {
-			tags : store.getTags(),
-			isRequesting : store.isTagsRequesting(),
-			isUpdating: store.isTagsUpdating()
-		}
-	} );
-
-	if ( isRequesting || isUpdating ) {
-		return <Spinner />;
-	}
+	const classes = useStyles();
+	// const [ stateTagValue, setTagValue ] = useState( '' );
+	//
+	// const { updateTags } = useDispatch( TAGS_STORE_NAME );
+	//
+	// const { tags, isRequesting, isUpdating } = useSelect( ( select ) => {
+	// 	const store = select( TAGS_STORE_NAME );
+	// 	return {
+	// 		tags : store.getTags(),
+	// 		isRequesting : store.isTagsRequesting(),
+	// 		isUpdating: store.isTagsUpdating()
+	// 	}
+	// } );
+	//
+	// if ( isRequesting || isUpdating ) {
+	// 	return <Spinner />;
+	// }
 
 	return (
 			<Fragment>
-				<h2>Reports</h2>
-				<ol>
-					{ tags.map( tag => <li>{ tag.tag_name }</li> ) }
-				</ol>
-				<TextField id="outlined-basic" label="Add Tags" variant="outlined" value={ stateTagValue } onChange={ ( event ) => setTagValue( event.target.value ) } />
-				<p onClick={ () => { updateTags( { tags : stateTagValue } ) } }>Add</p>
-				{ ( isUpdating ) && ( <Spinner /> ) }
+				<Card className={classes.container}><Chart type='line'/></Card>
+				<Card className={classes.container}><Chart type='bar'/></Card>
+				<Card className={classes.container}>
+					<Typography className={classes.kpiTitle} component="h1" color="textSecondary">{`KPI`}</Typography>
+					<Typography className={classes.kpiMetric} component="div" color="textSecondary">{`${Math.round(Math.random()*1000)/10}%`}</Typography>
+				</Card>
+				<Card className={classes.container}>
+					<Typography className={classes.kpiTitle} component="h1" color="textSecondary">{`KPI`}</Typography>
+					<Typography className={classes.kpiMetric} component="div" color="textSecondary">{`${Math.round(Math.random()*1000)/10}%`}</Typography>
+				</Card>
+				<Card className={classes.container}>
+					<Typography className={classes.kpiTitle} component="h1" color="textSecondary">{`KPI`}</Typography>
+					<Typography className={classes.kpiMetric} component="div" color="textSecondary">{`${Math.round(Math.random()*1000)/10}%`}</Typography>
+				</Card>
+				<Card className={classes.container}>
+					<Typography className={classes.kpiTitle} component="h1" color="textSecondary">{`KPI`}</Typography>
+					<Typography className={classes.kpiMetric} component="div" color="textSecondary">{`${Math.round(Math.random()*1000)/10}%`}</Typography>
+				</Card>
+
 			</Fragment>
 	);
 }
