09f9d70b5db5b6370f701acf684b3170eb75341b
Justin Sainton <justinsainton@gmail.com>
2020-10-08 13:42:55 -0700

    Update contacts components

--- src/components/layout/pages/contacts/contact-row-primary-item.js
+++ src/components/layout/pages/contacts/contact-row-primary-item.js
@@ -3,13 +3,54 @@
  */
 import { Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-// email > avatar, email, link,  status, actions,
+import { hasQueryArg } from '@wordpress/url'
+import { makeStyles } from '@material-ui/core/styles';
 
-export const ContactRowPrimaryItem = ( data ) => {
-	console.log( data );
+const useStyles = makeStyles((theme) => ({
+	contactRowImage: {
+		maxWidth: '55px',
+		borderRadius: '50%',
+		boxShadow: '1px 1px 4px #ccc',
+	},
+  }));
+
+const getSingleContactLink = ( id ) => {
+	return `/${id}`;
+}
+
+const getContactRowActions = () => {
+	return 'SlotFill row actions here';
+}
+
+const getContactRowStatus = ( data ) => {
+	if ( hasQueryArg( 'optin_status' ) ) {
+		return null;
+	}
+
+	const statuses = {
+		1 : __( 'Unconfirmed' ),
+		2 : __( 'Confirmed' ),
+		3 : __( 'Unsubscribed' ),
+		4 : __( 'Weekly' ),
+		5 : __( 'Monthly' ),
+		6 : __( 'Hard Bounce' ),
+		7 : __( 'Spam' ),
+		8 : __( 'Complained' ),
+	}
+
+	return ` - ${statuses[data.optin_status]}`;
+}
+
+export const ContactRowPrimaryItem = ( props ) => {
+	const classes = useStyles();
+	const { data } = props;
 	return (
 		<Fragment>
-			{data.ID}
+			<a href={ getSingleContactLink( data.ID ) }><img className={classes.contactRowImage} src={ data.gravatar } /></a>
+			<a href={ getSingleContactLink( data.ID ) }>{ data.email }</a>
+			{ getContactRowStatus( data ) }
+			<br />
+			{ getContactRowActions() }
 		</Fragment>
 	)
 }
--- src/components/layout/pages/contacts/index.js
+++ src/components/layout/pages/contacts/index.js
@@ -3,6 +3,7 @@
  */
 import { Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { useSelect, useDispatch } from '@wordpress/data'
 
 /**
  * Internal dependencies
@@ -13,68 +14,87 @@ import {
 import { ListTable } from '../../../core-ui/list-table/new'
 import { ContactRowPrimaryItem } from './contact-row-primary-item'
 
+const contactTableColumns = [
+	{
+		ID: 'email',
+		name: __( 'Email' ),
+		orderBy: 'email',
+		align: 'left',
+		cell: ({data, ID}) => {
+			return <ContactRowPrimaryItem data={data} />
+		}
+	},
+	{
+		ID: 'first_name',
+		name: __( 'First Name' ),
+		orderBy: 'first_name',
+		align: 'left',
+		cell: ({data}) => {
+			return data.first_name || ''
+		}
+	},
+	{
+		ID: 'last_name',
+		name: __( 'Last Name' ),
+		orderBy: 'last_name',
+		align: 'left',
+		cell: ({data}) => {
+			return data.last_name || ''
+		}
+	},
+	{
+		ID: 'username',
+		name: __( 'Username' ),
+		orderBy: 'username',
+		align: 'left',
+		cell: ({data}) => {
+			return data.meta && data.meta.user_login || ''
+		}
+	},
+	{
+		ID: 'owner',
+		name: __( 'Owner' ),
+		orderBy: 'owner_id',
+		align: 'left',
+		cell: ({data}) => {
+			return data.owner_id || '' // Need to resolve this to name/link
+		}
+	},
+	{
+		ID: 'date_created',
+		name: __( 'Date Created' ),
+		orderBy: 'date_created',
+		align: 'left',
+		cell: ({data}) => {
+			return data.date_created || '' // Need to resolve this to proper format
+		}
+	},
+];
+
 export const Contacts = ( props ) => {
+	const { items, totalItems, isRequesting } = useSelect( (select) => {
+		const store = select(CONTACTS_STORE_NAME);
+
+		return {
+			items: store.getItems(),
+			totalItems: store.getTotalItems(),
+			isRequesting: store.isItemsRequesting()
+		}
+	}, [] );
+
+	const { fetchItems } = useDispatch( CONTACTS_STORE_NAME );
+
 	return (
 		<Fragment>
 			<ListTable
-				storeName={ CONTACTS_STORE_NAME }
-				columns={[
-					{
-						ID: 'email',
-						name: 'Email',
-						orderBy: 'email',
-						align: 'left',
-						cell: ({data, ID}) => {
-							return <ContactRowPrimaryItem data={data} />
-						}
-					},
-					{
-						ID: 'first_name',
-						name: 'First Name',
-						orderBy: 'first_name',
-						align: 'left',
-						cell: ({data}) => {
-							return data.first_name || ''
-						}
-					},
-					{
-						ID: 'last_name',
-						name: 'Last Name',
-						orderBy: 'last_name',
-						align: 'left',
-						cell: ({data}) => {
-							return data.last_name || ''
-						}
-					},
-					{
-						ID: 'username',
-						name: 'Username',
-						orderBy: 'username',
-						align: 'left',
-						cell: ({data}) => {
-							return data.meta && data.meta.user_login || ''
-						}
-					},
-					{
-						ID: 'owner',
-						name: 'Owner',
-						orderBy: 'owner_id',
-						align: 'left',
-						cell: ({data}) => {
-							return data.owner_id || '' // Need to resolve this to name/link
-						}
-					},
-					{
-						ID: 'date_created',
-						name: 'Date Created',
-						orderBy: 'date_created',
-						align: 'left',
-						cell: ({data}) => {
-							return data.date_created || '' // Need to resolve this to proper format
-						}
-					},
-				]}
+				items={items}
+				defaultOrderBy={'date_created'}
+				defaultOrder={'asc'}
+				totalItems={totalItems}
+				fetchItems={fetchItems}
+				isRequesting={isRequesting}
+				columns={contactTableColumns}
 			/>
 		</Fragment>
-	);
+	)
 }
