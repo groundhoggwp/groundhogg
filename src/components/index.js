e037b7e439b261c6aaf4e5837eab886443dfbda8
Justin Sainton <justinsainton@gmail.com>
2020-10-08 09:36:16 -0700

    Revert "fix build issue"
    This reverts commit 6931919d7882b19e26915db74e941fd65d1bfc7e.

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export { PageLayout } from './layout';
 export { default as BottomBar } from './layout/bottom-bar';
 export { default as BulkJob } from './core-ui/bulk-job';
-// export { default as Buttons } from './core-ui/buttons';
+export { default as Buttons } from './core-ui/buttons';
 export { default as ListTable } from './core-ui/list-table';
 export { default as TagPicker } from './core-ui/tag-picker';
 export { default as VideoModal } from './core-ui/video-modal';
