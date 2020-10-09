1d65223ebde5cf2d6e20ab35e0a1a3276795f660
Nathan Sachs <>
2020-10-08 20:42:10 -0400

    Let's start the with drawer closed, it looks better anyway

--- src/components/layout/top-bar/index.js
+++ src/components/layout/top-bar/index.js
@@ -104,7 +104,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Dashboard(props) {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
   const handleDrawerOpen = () => {
     setOpen(true);
   };
