0eab1886a48d0c25263f956827192fba786c0efd
tobeyadr <gO0UsDYxRwhz>
2020-10-09 13:14:09 -0400

    add duplicate endpoint

--- api/v4/funnels-api.php
+++ api/v4/funnels-api.php
@@ -3,12 +3,51 @@
 namespace Groundhogg\Api\V4;
 
 // Exit if accessed directly
+use Groundhogg\Funnel;
+use WP_REST_Server;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 class Funnels_Api extends Base_Object_Api {
 
+	public function register_routes() {
+		parent::register_routes();
+
+		register_rest_route( self::NAME_SPACE, "/funnels/(?P<ID>\d+)/duplicate", [
+			[
+				'methods'             => WP_REST_Server::CREATABLE,
+				'callback'            => [ $this, 'duplicate' ],
+				'permission_callback' => [ $this, 'create_permissions_callback' ]
+			],
+		] );
+	}
+
+	/**
+	 * Duplicate the funnel
+	 *
+	 * @param \WP_REST_Request $request
+	 *
+	 * @return \WP_Error|\WP_REST_Response
+	 */
+	public function duplicate( \WP_REST_Request $request ){
+
+		$ID = absint( $request->get_param( 'ID' ) );
+		$funnel = new Funnel( $ID );
+
+		if ( ! $funnel->exists() ){
+			return self::ERROR_404();
+		}
+
+		$new_funnel = new Funnel();
+
+		$new_funnel->import( $funnel->export() );
+
+		return self::SUCCESS_RESPONSE( [
+			'item' => $new_funnel
+		] );
+	}
 
 	/**
 	 * The name of the table resource to use
