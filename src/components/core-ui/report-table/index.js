c19785748b82df90d9c673d121d1f795c69b9e59
Nathan Sachs <>
2020-11-11 21:08:36 -0500

    Passing HTML directly as an error message

--- src/components/core-ui/report-table/index.js
+++ src/components/core-ui/report-table/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography'
 import Card from "@material-ui/core/Card";
 import _ from 'lodash';
 
@@ -50,21 +51,22 @@ const ReportTable = ({title, data, gridColumnStart, gridColumnEnd, gridRowStart,
     return (
       <Card className={classes.root}>
         <div className={classes.title}>{title}</div>
-        <p>{data.no_data}</p>
+        <Typography className={classes.descriptionCheckbox} variant="p" component="p" dangerouslySetInnerHTML={{ __html: data.no_data }}  />
       </Card>
     );
   }
 
   // const tableLabels = data.chart.label.filter(label => !label.includes('Funnel'))
   const tableLabels = data.chart.label;
-  console.log(data.chart.label, tableLabels)
+  // const dataset
+  // console.log(data.chart.label, data.chart.data, tableLabels)
 
   // Manual width calc is required because each container is dynamic and data grid can't use % then
   let columnWidth = ((window.innerWidth/2)/(tableLabels.length*2));
   if(fullWidth){
     columnWidth = ((window.innerWidth)/(tableLabels.length*2));
   }
-  console.log(title, columnWidth)
+  // console.log(title, columnWidth)
 
   const columns = Object.keys(data.chart.data[0]).map((label, i)=>{
     // The server data model isn't ideal for this component, sometimes labels are valid and exist
@@ -80,6 +82,7 @@ const ReportTable = ({title, data, gridColumnStart, gridColumnEnd, gridRowStart,
   });
 
 
+  // Remove funnels and other garbage from here
   const rows = data.chart.data.map((data, i)=>{
     let row = { id: i, [columns[0]['field']]: data.label, [columns[1]['field']]: data.data};
 
