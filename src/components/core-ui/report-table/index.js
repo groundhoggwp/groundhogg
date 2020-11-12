f6140c8655cebb7fdae6fd3c1e7a1c75b5dc2e22
Nathan Sachs <>
2020-11-11 20:02:02 -0500

    Major re-work of the report table, made it smarter to handle some ugly data

--- src/components/core-ui/report-table/index.js
+++ src/components/core-ui/report-table/index.js
@@ -2,6 +2,19 @@ import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from "@material-ui/core/Card";
+import _ from 'lodash';
+
+// Bring this into a
+function titleCase(str) {
+   var splitStr = str.toLowerCase().split(' ');
+   for (var i = 0; i < splitStr.length; i++) {
+       // You do not need to check if i is larger than splitStr length, as your for does that for you
+       // Assign it back to the array
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+   }
+   // Directly return the joined string
+   return splitStr.join(' ');
+}
 
 const ReportTable = ({title, data, gridColumnStart, gridColumnEnd, gridRowStart, gridRowEnd, fullWidth}) => {
   const useStyles = makeStyles({
@@ -42,28 +55,36 @@ const ReportTable = ({title, data, gridColumnStart, gridColumnEnd, gridRowStart,
     );
   }
 
+  // const tableLabels = data.chart.label.filter(label => !label.includes('Funnel'))
+  const tableLabels = data.chart.label;
+  console.log(data.chart.label, tableLabels)
+
   // Manual width calc is required because each container is dynamic and data grid can't use % then
-  let columnWidth = ((window.innerWidth/2)/(data.chart.label.length*2));
+  let columnWidth = ((window.innerWidth/2)/(tableLabels.length*2));
   if(fullWidth){
-    columnWidth = ((window.innerWidth)/(data.chart.label.length*2));
+    columnWidth = ((window.innerWidth)/(tableLabels.length*2));
   }
-  let columns = data.chart.label.map((label)=>{
+  console.log(title, columnWidth)
+
+  const columns = Object.keys(data.chart.data[0]).map((label, i)=>{
+    // The server data model isn't ideal for this component, sometimes labels are valid and exist
+    // Sometimes they need to be inferred from row data
+    if(tableLabels[i]){
+      label = tableLabels[i]
+    }
     if(typeof(value)=== 'object'){
       label = 'Conversation Rate';
     }
 
-    return { field: label, headerName: label, width: columnWidth, align: 'left', sortable: true }
+    return { field: label, headerName: _.capitalize(label), width: columnWidth, align: 'left', sortable: true }
   });
-
-  if(data.chart.data[0].percentage && !data.chart.label.includes('Percentage')){
-    columns.push({ field: 'Percentage', headerName: 'Percentage', width: columnWidth, align: 'left', sortable: true })
-  }
 
 
   const rows = data.chart.data.map((data, i)=>{
     let row = { id: i, [columns[0]['field']]: data.label, [columns[1]['field']]: data.data};
+
     if(data.percentage){
-      row['Percentage'] = data.percentage;
+      row[columns[2]['field']] = data.percentage;
     }
     return row
   })
@@ -71,7 +92,7 @@ const ReportTable = ({title, data, gridColumnStart, gridColumnEnd, gridRowStart,
   return (
     <Card className={classes.root}>
       <div className={classes.title}>{title}</div>
-      <DataGrid rows={rows} columns={columns} pageSize={6} checkboxSelection={false} />
+      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection={false} />
     </Card>
   );
 }
