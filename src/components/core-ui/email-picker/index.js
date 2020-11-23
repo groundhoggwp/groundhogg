b84a93e3f2d2339432532bb73562cef6ba3d2f34
Dhrumit07 <dhrumit07@gmail.com>
2020-11-23 13:01:50 -0500

    Updated broadcast with the tag pickers and email picker to set data

--- src/components/core-ui/email-picker/index.js
+++ src/components/core-ui/email-picker/index.js
@@ -1,64 +1,65 @@
 import AsyncCreatableSelect from 'react-select/async-creatable'
 import AsyncSelect from 'react-select/async'
-import apiFetch from "@wordpress/api-fetch";
-import {NAMESPACE} from "data/constants";
-import {useEffect} from "@wordpress/element";
+import apiFetch from '@wordpress/api-fetch'
+import { NAMESPACE } from 'data/constants'
+import { useEffect } from '@wordpress/element'
 import { parseArgs } from 'utils/core'
+import { addQueryArgs } from '@wordpress/url'
 
-
-const FaIcon = ({classes}) => {
-    return <i className={'fa ' + classes.map(c => 'fa-' + c).join(' ')}></i>
+const FaIcon = ({ classes }) => {
+  return <i className={'fa ' + classes.map(c => 'fa-' + c).join(' ')}></i>
 }
 
-const EmailPicker = ({selectProps, onChange, value, isCreatable}) => {
+const EmailPicker = ({ selectProps, onChange, value, isCreatable }) => {
 
-    selectProps = parseArgs(selectProps || {}, {
-        cacheOptions: true,
-        isMulti: false,
-        ignoreCase: true,
-        isClearable: true,
-        // defaultOptions: [],
+  selectProps = parseArgs(selectProps || {}, {
+    cacheOptions: true,
+    isMulti: false,
+    ignoreCase: true,
+    isClearable: true,
+    // defaultOptions: [],
+  })
+
+  const promiseOptions = inputValue => new Promise(resolve => {
+    let query = {
+      where: {
+        status: 'ready'
+      }
+    }
+
+    apiFetch({
+      method: 'GET',
+      path: addQueryArgs(NAMESPACE + '/emails?search=' + inputValue, query)
+      // data: data
+    }).then(({ items }) => {
+      resolve(items.map((item) => {
+        return ({ value: item.ID, label: item.data.title + `(${item.data.status})` })
+      }))
     })
+  })
 
-    const promiseOptions = inputValue  => new Promise(resolve => {
-        apiFetch({
-            method: 'GET',
-            path: NAMESPACE + '/emails?search=' + inputValue,
-            // body : {
-            //     "where" :{
-            //         "status" : "ready"
-            //     }
-            // }
-        }).then(({items}) => {
-            console.log(items);
-            resolve(items.map((item) => {
-                return ({value: item.ID, label: item.data.title + `(${item.data.status})`})
-            }));
-        })
-    });
+  // const TagSelect = isCreatable ? AsyncCreatableSelect : AsyncSelect
+  const TagSelect = AsyncSelect
+  // value = value.map(item  => {return ({value: item}) });
 
-    // const TagSelect = isCreatable ? AsyncCreatableSelect : AsyncSelect
-    const TagSelect =  AsyncSelect;
-    // value = value.map(item  => {return ({value: item}) });
-
-    return (
-        // <TagSelect
-        //     {...selectProps}
-        //     defaultOptions={promiseOptions}
-        //     loadOptions={promiseOptions}
-        //     onChange={ onChange }
-        //     value={ value }
-        // />
-        //
-        <AsyncSelect
-            {...selectProps}
-            cacheOptions
-            defaultOptions
-            loadOptions={promiseOptions}
-            onChange ={onChange}
-            value ={value}
-        />
-    )
+  return (
+    // <TagSelect
+    //     {...selectProps}
+    //     defaultOptions={promiseOptions}
+    //     loadOptions={promiseOptions}
+    //     onChange={ onChange }
+    //     value={ value }
+    // />
+    //
+    <AsyncSelect
+      {...selectProps}
+      cacheOptions
+      defaultOptions
+      loadOptions={promiseOptions}
+      onChange={onChange}
+      value={value}
+    />
+  )
 }
 
-export default EmailPicker;
+export default EmailPicker
