277f757464fac17bdb8062e937a83b5e4e482dcc
Adrian Tobey <adrian.methoss@gmail.com>
2020-11-26 16:02:20 -0500

    Support wacky protocols for use with replacement codes.

--- includes/filters.php
+++ includes/filters.php
@@ -242,8 +242,45 @@ add_filter( 'admin_footer_text', __NAMESPACE__ . '\add_bug_report_prompt' );
 
 add_filter( 'groundhogg/admin/emails/sanitize_email_content', __NAMESPACE__ . '\safe_css_filter_rgb_to_hex', 10 );
 add_filter( 'groundhogg/admin/emails/sanitize_email_content', __NAMESPACE__ . '\add_safe_style_attributes_to_email', 10 );
-add_filter( 'groundhogg/admin/emails/sanitize_email_content', 'wp_kses_post', 11 );
+add_filter( 'groundhogg/admin/emails/sanitize_email_content', __NAMESPACE__ . '\kses_wrapper', 11 );
 
+function kses_wrapper( $content ){
+
+	// Basic protocols
+	$basic_protocols = [ 'http', 'https', 'mailto','mms', 'sms', 'svn', 'tel', 'fax' ];
+
+	// Weird protocols for replacements compatibility
+	$wacky_protocols = [
+		'{confirmation_link_raw.{auto_login_link.https',
+		'{confirmation_link_raw.{auto_login_link.https',
+		'{confirmation_link.{auto_login_link.http',
+		'{confirmation_link.{auto_login_link.http',
+		'{confirmation_link_raw.https',
+		'{confirmation_link_raw.http',
+		'{confirmation_link.https',
+		'{confirmation_link.http',
+		'{auto_login_link.{confirmation_link_raw.https',
+		'{auto_login_link.{confirmation_link_raw.http',
+		'{auto_login_link.https',
+		'{auto_login_link.http',
+	];
+
+	return wp_kses( $content, 'post', array_merge( $basic_protocols, $wacky_protocols ) );
+}
+
+/**
+ * This is pretty hacky, do better in the future.
+ *
+ * @param $protocols array
+ */
+function replacement_code_protocols_compat_hack( $protocols ){
+
+	$protocols[] = '{confirmation_link_raw.{auto_login_link.https';
+//	$protocols[] = '{confirmation_link_raw.{auto_login_link.{superlink}}';
+	$protocols[] = '{confirmation_link_raw.{auto_login_link.http';
+
+	return $protocols;
+}
 /**
  * Add some filters....
  *
