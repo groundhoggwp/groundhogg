a36adf7c6033c25a376599509cb969a0039786e3
Dhrumit07 <dhrumit07@gmail.com>
2020-11-27 09:18:45 -0500

    Added Contact Info page with update functionality

---
+++ src/components/layout/pages/contacts/general-info.js
@@ -0,0 +1,269 @@
+import { Fragment } from '@wordpress/element'
+import { ContactPanel } from 'components/layout/pages/contacts/contact-panel'
+import React from 'react'
+
+import { __ } from '@wordpress/i18n'
+
+export const GeneralInfo = (props) => {
+
+  const info = [
+    {
+      id: 'contact_info',
+      tab: 'general_info',
+      title: __('Contact Information', 'groundhogg'),
+      fields: [
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'first_name',
+          label: __('First Name', 'groundhogg'),
+          section: 'contact_info',
+          dataType: 'data',
+          type: 'input',
+          sm: '12',
+          md: '6',
+          lg: '6'
+        },
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'last_name',
+          label: __('Last Name', 'groundhogg'),
+          dataType: 'data',
+          section: 'contact_info',
+          type: 'input',
+          sm: '12',
+          md: '6',
+          lg: '6'
+        },
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'email',
+          label: __('Email', 'groundhogg'),
+          dataType: 'data',
+          section: 'contact_info',
+          type: 'input',
+          sm: '12',
+          md: '12',
+          lg: '12'
+        },
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'primary_phone',
+          label: __('Primary Phone', 'groundhogg'),
+          dataType: 'meta',
+          section: 'contact_info',
+          type: 'input',
+          sm: '12',
+          md: '6',
+          lg: '6'
+        },
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'primary_phone_extension',
+          label: __('Primary Phone Extension', 'groundhogg'),
+          dataType: 'data',
+          section: 'contact_info',
+          type: 'input',
+          sm: '12',
+          md: '6',
+          lg: '6'
+        }
+      ],
+
+    }, {
+      id: 'user_account',
+      tab: 'general_info',
+      title: __('User Account', 'groundhogg'),
+      fields: [
+        {
+          defaultValue: '',
+          desc: 'The primary user to reference for contact information.',
+          id: 'gh_primary_user',
+          label: 'Primary User',
+          section: 'general_other',
+          type: 'input',
+        }
+      ],
+
+    }, {
+      id: 'personal_info',
+      tab: 'general_info',
+      title: __('Personal Info', 'groundhogg'),
+      fields: [
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'birthday',
+          label: __('Birthday', 'groundhogg'),
+          section: 'personal_info',
+          type: 'input',
+          dataType: 'meta',
+          sm: '12',
+          md: '12',
+          lg: '12'
+        },
+      ],
+
+    }, {
+      id: 'company_info',
+      tab: 'general_info',
+      title: __('Company Info', 'groundhogg'),
+      fields: [
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'company_name',
+          label: __('Company Name', 'groundhogg'),
+          section: 'company_info',
+          type: 'input',
+          dataType: 'meta',
+          sm: '12',
+          md: '12',
+          lg: '12'
+        },
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'job_title',
+          label: __('Job Title', 'groundhogg'),
+          section: 'company_info',
+          type: 'input',
+          dataType: 'meta',
+          sm: '12',
+          md: '12',
+          lg: '12'
+        },
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'company_address',
+          label: __('Company Address', 'groundhogg'),
+          section: 'personal_info',
+          type: 'input',
+          dataType: 'meta',
+          sm: '12',
+          md: '12',
+          lg: '12'
+        },
+      ],
+
+    },
+    {
+      id: 'location',
+      tab: 'general_info',
+      title: __('Location', 'groundhogg'),
+
+      fields: [
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'street_address_1',
+          label: __('Street Address 1', 'groundhogg'),
+          section: 'location',
+          type: 'input',
+          dataType: 'meta',
+          sm: '12',
+          md: '6',
+          lg: '6'
+        },
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'street_address_2',
+          label: __('Street Address 2', 'groundhogg'),
+          section: 'location',
+          type: 'input',
+          dataType: 'meta',
+          sm: '12',
+          md: '6',
+          lg: '6'
+        },
+
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'postal_zip',
+          label: __('Postal/Zip Code', 'groundhogg'),
+          section: 'location',
+          type: 'input',
+          dataType: 'meta',
+          sm: '12',
+          md: '6',
+          lg: '6'
+        },
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'city',
+          label: __('city', 'groundhogg'),
+          section: 'location',
+          type: 'input',
+          dataType: 'meta',
+          sm: '12',
+          md: '6',
+          lg: '6'
+        },
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'region',
+          label: __('State/Province', 'groundhogg'),
+          section: 'location',
+          type: 'input',
+          dataType: 'meta',
+          sm: '12',
+          md: '6',
+          lg: '6'
+        },
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'region',
+          label: __('country', 'groundhogg'),
+          section: 'location',
+          type: 'input',
+          dataType: 'meta',
+          sm: '12',
+          md: '6',
+          lg: '6'
+
+        },
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'ip_address',
+          label: __('IP Address', 'groundhogg'),
+          section: 'location',
+          type: 'input',
+          dataType: 'meta',
+          sm: '12',
+          md: '12',
+          lg: '12'
+        },
+        {
+          defaultValue: '',
+          desc: '',
+          id: 'time_zone',
+          label: __('Time Zone', 'groundhogg'),
+          section: 'location',
+          type: 'input',
+          dataType: 'meta',
+          sm: '12',
+          md: '12',
+          lg: '12'
+        },
+      ],
+    }
+  ]
+
+  return (
+    <Fragment>
+      <ContactPanel section={info} contact={props.contact}/>
+    </Fragment>
+  )
+
+}
--- src/components/layout/pages/contacts/single-view.js
+++ src/components/layout/pages/contacts/single-view.js
@@ -1,20 +1,161 @@
+import React from 'react'
 import { Fragment } from '@wordpress/element'
 import {
-	useParams
-} from "react-router-dom";
+  Link,
+  useParams
+} from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import ContactTimeline from './contact-timeline'
+import TabPanel from 'components/core-ui/tab-panel'
+import { filter, forEach } from 'lodash'
+import { applyFilters } from '@wordpress/hooks'
+import { SettingsPanel } from 'components/layout/pages/settings/settings-panel'
+import { Divider } from '@material-ui/core'
+import { useSelect } from '@wordpress/data'
+import { CONTACTS_STORE_NAME } from 'data/contacts'
 
-export const SingleView = ( props ) => {
-	let { id } = useParams();
 
-	const {
-		getItem,
-		updateItem,
-		deleteItem,
-	} = props;
+import MailOutlineIcon from '@material-ui/icons/MailOutline'
+import { ContactPanel } from 'components/layout/pages/contacts/contact-panel'
+import TextField from '@material-ui/core/TextField'
+import {GeneralInfo}  from './general-info'
 
-	return (
-		<Fragment>
-			<p>Single view: Contact ID: {id}</p>
-		</Fragment>
-	)
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  contactImage: {
+    maxWidth: '150px',
+    borderRadius: '50%',
+    // boxShadow: '1px 1px 4px #ccc',
+
+  }
+}))
+
+const ContactMeta = (props) => {
+
+}
+
+const CustomInfo = (props) => {
+
+  return (
+    <h1> custom </h1>
+  )
+
+}
+
+//
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     width: '100%',
+//     maxWidth: 360,
+//     backgroundColor: theme.palette.background.paper,
+//   },
+// }));
+
+
+const Segmentation = (props) => {
+  return (
+    <h1> Segementation </h1>
+  )
+
+}
+
+export const ContactSection = ({ section }) => {
+  return (
+    <div>
+      <h3>{section.title}</h3>
+      <Divider/>
+      <section.component/>
+    </div>
+  )
+
+}
+
+export const SingleView = (props) => {
+  const classes = useStyles()
+  let { id } = useParams()
+
+  const { match, history } = props
+
+  const { contact } = useSelect((select) => {
+    const store = select(CONTACTS_STORE_NAME)
+    return {
+      contact: store.getItem(id)
+    }
+  }, [])
+
+  if (Object.keys(contact).length === 0 && contact.constructor === Object) {
+    return (<h1> Loading... </h1>)
+  }
+
+  const { data, meta } = contact
+
+  const tabs = [
+    {
+      label: 'General Info ',
+      route: 'general',
+      component: () => {
+        return (
+          <GeneralInfo contact={contact}/>
+        )
+      }
+    },
+    {
+      label: 'Custom Info',
+      route: 'custom',
+      component: () => {
+        return (
+          <CustomInfo contact={contact}/>
+        )
+      }
+    },
+    {
+      label: 'Segmentation',
+      route: 'segmentation',
+      component: () => {
+        return (
+          <Segmentation contact={contact}/>
+        )
+      }
+    }
+  ]
+
+
+  return (
+    <Fragment>
+      {/*<p>Single view: Contact ID: {id}</p>*/}
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={3} lg={3}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={12} lg={12}>
+              <img className={classes.contactImage}
+                   src={meta.profile_picture}/>
+              <h3> {data.first_name + ' ' + data.last_name} </h3>
+              <p> {data.email}</p>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={7} lg={7}>
+
+            <Fragment>
+              <TabPanel tabs={tabs} enableRouting={false} history={history} match={match}/>
+            </Fragment>
+          
+        </Grid>
+        <Grid item xs={12} md={2} lg={2}>
+          <Paper>
+            <ContactTimeline/>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Fragment>
+  )
 }
