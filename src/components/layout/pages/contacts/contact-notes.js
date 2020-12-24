ba870b21696d3d4d17680a92ba97c2c698b19980
Dhrumit <dhrumit07@gmail.com>
2020-12-23 19:07:31 -0500

    Tried to add list view with add more functionality using extra library. (Not working as expected)
    Added functionality to delete and edit notes.

--- src/components/layout/pages/contacts/contact-notes.js
+++ src/components/layout/pages/contacts/contact-notes.js
@@ -1,4 +1,4 @@
-import {Fragment, render, useState} from '@wordpress/element'
+import {Fragment, render, useCallback, useEffect, useState} from '@wordpress/element'
 import {ListTable} from 'components/core-ui/list-table/new'
 import React from 'react'
 import TextField from "@material-ui/core/TextField";
@@ -10,35 +10,151 @@ import {useParams} from "react-router-dom";
 import Box from "@material-ui/core/Box";
 import LocalOfferIcon from "@material-ui/icons/LocalOffer";
 import {Button} from "@material-ui/core";
-import {addNotification} from "utils";
-import { __ } from '@wordpress/i18n'
+import {addNotification, useKeyPress} from "utils";
+import {__} from '@wordpress/i18n'
+import {ListView} from "components/core-ui/list-view";
+import SettingsIcon from "@material-ui/icons/Settings";
+import RowActions from "components/core-ui/row-actions";
 
 const notesTableColumn = [
+
     {
-        ID: 'tag_id',
-        name: 'ID',
-        orderBy: 'tag_id',
-        align: 'right',
-        cell: ({data}) => {
-            return data.ID
-        },
-    },
-    {
-        ID: 'name',
-        name: 'Name',
-        orderBy: 'tag_name',
+        ID: 'content',
+        name: 'Content',
+        orderBy: '',
         align: 'left',
         cell: ({data}) => {
             return <>{data.content}</>
         },
     },
+
+    {
+        ID: 'date',
+        name: 'Date',
+        orderBy: '',
+        align: 'left',
+        cell: ({data}) => {
+            return <>{data.date_created}</>
+        },
+    },
+    {
+        ID: 'actions',
+        name: <span><SettingsIcon/> { 'Actions' }</span>,
+        align: 'right',
+        cell: ({ ID, data, openQuickEdit }) => {
+
+            const { deleteItem } = useDispatch(NOTES_STORE_NAME)
+
+            const handleEdit = () => {
+                openQuickEdit()
+            }
+
+            const handleDelete = (ID) => {
+                deleteItem(ID)
+            }
+
+            return <>
+                <RowActions
+                    onEdit={ openQuickEdit }
+                    onDelete={ () => handleDelete(ID) }
+                />
+            </>
+        },
+    },
+
 ];
+
+
+/**
+ * Handle the table quick edit
+ *
+ * @param ID
+ * @param data
+ * @param exitQuickEdit
+ * @returns {*}
+ * @constructor
+ */
+const NotesQuickEdit = ({ ID, data, exitQuickEdit }) => {
+
+    const { updateItem } = useDispatch(NOTES_STORE_NAME)
+    const [tempState, setTempState] = useState({
+        ...data,
+    })
+
+    // Exit quick edit
+    useKeyPress(27, null, () => {
+        exitQuickEdit()
+    })
+
+    /**
+     * Handle pressing enter in the tag name
+     *
+     * @param keyCode
+     */
+    const handleOnKeydown = ({ keyCode }) => {
+        switch (keyCode) {
+            case 13:
+                commitChanges()
+        }
+    }
+
+    /**
+     * Store the changes in a temp state
+     *
+     * @param atts
+     */
+    const handleOnChange = (atts) => {
+        setTempState({
+            ...tempState,
+            ...atts,
+        })
+    }
+
+    /**
+     * Commit the changes
+     */
+    const commitChanges = () => {
+        updateItem(ID, {
+            data: tempState,
+        })
+        exitQuickEdit()
+    }
+
+    return (
+        <Box display={ 'flex' } justifyContent={ 'space-between' }>
+            <Box flexGrow={ 2 }>
+                <TextField
+                    id="content"
+                    label={ 'Content' }
+                    multiline
+                    fullWidth
+                    rows={ 2 }
+                    value={ tempState && tempState.content }
+                    onChange={ (e) => handleOnChange(
+                        { content: e.target.value }) }
+                    variant="outlined"
+                />
+            </Box>
+            <Box flexGrow={ 1 }>
+                <Box display={ 'flex' } justifyContent={ 'flex-end' }>
+                    <Button variant="contained" color="primary" onClick={ commitChanges }>
+                        { 'Save Changes' }
+                    </Button>
+                    <Button variant="contained" onClick={ exitQuickEdit }>
+                        { 'Cancel' }
+                    </Button>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+
+
 export const ContactNotes = ({contact}) => {
 
 
     //Get notes from the db
-
-
 
 
     let {id} = useParams()
@@ -70,23 +186,43 @@ export const ContactNotes = ({contact}) => {
                 context: "user",
                 content: note,
                 object_type: "contact",
-                user_id : contact.data.user_id
+                user_id: contact.data.user_id
             }
         });
         setNote('')
-        addNotification( {message : __( 'Contact note added successfully.' ) , type : 'success' })
+        addNotification({message: __('Contact note added successfully.'), type: 'success'})
 
     }
 
     const fetchNotes = (obj) => {
         fetchItems({
-            where: {
-                object_id: id,
-                object_type: 'contact'
+            ...obj,
+            ...{
+                where: {
+                    object_id: id,
+                    object_type: 'contact'
+                }
             }
         })
+        // fetchItems(
+        //     {
+        //         where: {
+        //             object_id: id,
+        //             object_type: 'contact'
+        //         }
+        //     }
+        // )
         return {}
     }
+
+    const DisplayRecord = ({index, data}) => {
+
+        return <h1>{data.data.content}</h1>
+
+    }
+
+    //
+    // table
 
     return (
         <Fragment>
@@ -97,14 +233,14 @@ export const ContactNotes = ({contact}) => {
                 fullWidth
                 size="small"
                 rows={3}
-                value={ note }
+                value={note}
                 onChange={(e) => setNote(e.target.value)}
                 variant="outlined"
             />
 
             <Button onClick={addContactNotes}> Add Note </Button>
 
-            <Box flexGrow={1}>
+            {/*<Box flexGrow={1}>*/}
                 <ListTable
                     items={items}
                     // defaultOrderBy={'ID'}
@@ -113,11 +249,16 @@ export const ContactNotes = ({contact}) => {
                     fetchItems={fetchNotes}
                     isRequesting={isRequesting}
                     columns={notesTableColumn}
+                    QuickEdit={ NotesQuickEdit }
 
                 />
-            </Box>
+            {/*</Box>*/}
 
-
+            {/*<ListView items={items}*/}
+            {/*          fetchItems={fetchNotes}*/}
+            {/*          DisplayRecord={DisplayRecord}*/}
+            {/*          // defaultOrderBy={'ID'}*/}
+            {/*          defaultOrder={'desc'}/>*/}
         </Fragment>
     )
 
