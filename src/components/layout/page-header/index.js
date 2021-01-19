e9788ba1f9115d2a6bd3a1318148283bef8c9ea1
Nathan <n.sachs.7@gmail.com>
2021-01-19 14:29:47 -0500

    Page header comp added

---
+++ src/components/layout/page-header/index.js
@@ -0,0 +1,133 @@
+import React, {
+  useRef,
+  useState
+} from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import {
+  Breadcrumbs,
+  Button,
+  Grid,
+  Link,
+  Menu,
+  MenuItem,
+  SvgIcon,
+  Typography,
+  makeStyles
+} from '@material-ui/core';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import { Calendar as CalendarIcon } from 'react-feather';
+
+const timeRanges = [
+  {
+    value: 'today',
+    text: 'Today'
+  },
+  {
+    value: 'yesterday',
+    text: 'Yesterday'
+  },
+  {
+    value: 'last_30_days',
+    text: 'Last 30 days'
+  },
+  {
+    value: 'last_year',
+    text: 'Last year'
+  }
+];
+
+const useStyles = makeStyles(() => ({
+  root: {}
+}));
+
+const PageHeader = ({ className, page, ...rest }) => {
+  const classes = useStyles();
+  const actionRef = useRef(null);
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const [timeRange, setTimeRange] = useState(timeRanges[2].text);
+
+  console.log(rest, page.label)
+
+  return (
+    <Grid
+      container
+      spacing={3}
+      justify="space-between"
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <Grid item>
+        <Breadcrumbs
+          separator={<NavigateNextIcon fontSize="small" />}
+          aria-label="breadcrumb"
+        >
+          <Link
+            variant="body1"
+            color="inherit"
+            to="/app"
+            component={RouterLink}
+          >
+            {page.label}
+          </Link>
+          {/*<Typography
+            variant="body1"
+            color="textPrimary"
+          >
+            Reports
+          </Typography>*/}
+        </Breadcrumbs>
+
+        {/*<Typography
+          variant="h3"
+          color="textPrimary"
+        >
+          Here&apos;s what&apos;s happening
+        </Typography>*/}
+      </Grid>
+      <Grid item>
+        <Button
+          ref={actionRef}
+          onClick={() => setMenuOpen(true)}
+          startIcon={
+            <SvgIcon fontSize="small">
+              <CalendarIcon />
+            </SvgIcon>
+          }
+        >
+          {timeRange}
+        </Button>
+        <Menu
+          anchorEl={actionRef.current}
+          onClose={() => setMenuOpen(false)}
+          open={isMenuOpen}
+          getContentAnchorEl={null}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center'
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center'
+          }}
+        >
+          {timeRanges.map((_timeRange) => (
+            <MenuItem
+              key={_timeRange.value}
+              onClick={() => setTimeRange(_timeRange.text)}
+            >
+              {_timeRange.text}
+            </MenuItem>
+          ))}
+        </Menu>
+      </Grid>
+    </Grid>
+  );
+};
+
+PageHeader.propTypes = {
+  className: PropTypes.string
+};
+
+export default PageHeader;
