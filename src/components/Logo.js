cc5e001f8f47445661d1075f09d224af296d88f3
Nathan <n.sachs.7@gmail.com>
2021-01-19 13:44:48 -0500

    GH logo added

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ const Logo = (props) => {
   return (
     <img
       alt="Logo"
-      src="/static/logo.svg"
+      src="http://localhost/wordpress/wp-content/plugins/core/assets//images/phil-340x340.png"
       {...props}
     />
   );
