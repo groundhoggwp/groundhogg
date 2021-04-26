809e0afc08e1f189ec18a7a571d22586a3675272
Corey Maass <corey@corey.local>
2021-04-26 16:08:34 -0400

    More button made small.

--- src/components/layout/pages/funnelEditor/components/FunnelAppBar/index.js
+++ src/components/layout/pages/funnelEditor/components/FunnelAppBar/index.js
@@ -1,51 +1,49 @@
-import AppBar from '@material-ui/core/AppBar'
-import { FUNNELS_STORE_NAME } from 'data/funnels'
-import { useSelect } from '@wordpress/data'
-import Toolbar from '@material-ui/core/Toolbar'
-import makeStyles from '@material-ui/core/styles/makeStyles'
-import Box from '@material-ui/core/Box'
-import Button from '@material-ui/core/Button'
+import AppBar from '@material-ui/core/AppBar';
+import { FUNNELS_STORE_NAME } from 'data/funnels';
+import { useSelect } from '@wordpress/data';
+import Toolbar from '@material-ui/core/Toolbar';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
-import FunnelTitleEdit from '../FunnelTitleEdit'
-import FunnelStatus from '../FunnelStatus'
+import FunnelTitleEdit from '../FunnelTitleEdit';
+import FunnelStatus from '../FunnelStatus';
 
 const useStyles = makeStyles((theme) => ({
-  appBar: {
-    '& .MuiAppBar-colorPrimary': {
-      color: '#000',
-      backgroundColor: '#ffffff'
-    }
-  }
-}))
+	appBar: {
+		'& .MuiAppBar-colorPrimary': {
+			color: '#000',
+			backgroundColor: '#ffffff',
+		},
+	},
+}));
 
 export default ({ funnel, width }) => {
+	const classes = useStyles();
 
-  const classes = useStyles()
+	const { id, data } = {
+		id: funnel.ID,
+		data: funnel.data,
+	};
 
-  const { id, data } = {
-    id: funnel.ID,
-    data: funnel.data
-  }
-
-  return (
-    <div className={classes.appBar}>
-      <AppBar style={{ top: 32, width: width }}>
-        <Toolbar>
-          <Box display={'flex'} flexGrow={1}>
-            <Box>
-              <FunnelTitleEdit id={id} data={data}/>
-            </Box>
-            <Button  variant="outlined">
-              <MoreHorizIcon/>
-            </Button>
-            <Box display={ 'flex' }  flexGrow={1} justifyContent={ 'flex-end' }>
-              <FunnelStatus id={id} data={data}/>
-            </Box>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </div>
-  )
-
-}
+	return (
+		<div className={classes.appBar}>
+			<AppBar style={{ top: 32, width: width }}>
+				<Toolbar>
+					<Box display={'flex'} flexGrow={1}>
+						<Box>
+							<FunnelTitleEdit id={id} data={data} />
+						</Box>
+						<Button variant="outlined" size={'small'}>
+							<MoreHorizIcon />
+						</Button>
+						<Box display={'flex'} flexGrow={1} justifyContent={'flex-end'}>
+							<FunnelStatus id={id} data={data} />
+						</Box>
+					</Box>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
+};
