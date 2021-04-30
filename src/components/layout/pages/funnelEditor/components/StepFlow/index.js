465e09b60bc59e700a5185daba7523dcbd385c72
Corey Maass <corey@corey.local>
2021-04-30 15:14:53 -0400

    Preliminary drag and drop finally working!

--- src/components/layout/pages/funnelEditor/components/StepFlow/index.js
+++ src/components/layout/pages/funnelEditor/components/StepFlow/index.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { makeStyles } from "@material-ui/core/styles";
 import { unSlash } from "utils/core";
 import { getStepType, useStepType } from "data/step-type-registry";
@@ -62,26 +64,97 @@ export default (props) => {
   );
 };
 
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: 10 * 2,
+  margin: `0 0 ${10}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: 10,
+  width: 250,
+});
+
 const MainPath = ({ steps, edges }) => {
   window.console.log("MainPath");
-  const stepPath = processPath(steps, edges);
+
+  const [stepPath, updateStepPath] = useState(processPath(steps, edges));
+
+  function onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      stepPath,
+      result.source.index,
+      result.destination.index
+    );
+
+    updateStepPath(items);
+  }
 
   return (
-    <>
-      {stepPath.map((step) => {
-        const { StepIcon, StepRead, StepFlow, StepName } = useStepType(
-          step.data.step_type
-        );
-
-        return (
-          <StepFlow
-            icon={StepIcon}
-            name={StepName}
-            read={<StepRead {...step} />}
-          />
-        );
-      })}
-    </>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="droppable">
+        {(provided, snapshot) => (
+          <div
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            // style={getListStyle(snapshot.isDraggingOver)}
+          >
+            {stepPath.map((step, index) => {
+              const { StepIcon, StepRead, StepFlow, StepName } = useStepType(
+                step.data.step_type
+              );
+              return (
+                <Draggable
+                  key={step.ID}
+                  draggableId={String(step.ID)}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      // style={getItemStyle(
+                      //   snapshot.isDragging,
+                      //   provided.draggableProps.style
+                      // )}
+                    >
+                      <StepFlow
+                        icon={StepIcon}
+                        name={StepName}
+                        read={<StepRead {...step} />}
+                      />
+                    </div>
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
