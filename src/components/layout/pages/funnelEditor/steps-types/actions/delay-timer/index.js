015de402e6fe9dab4394e9dd4d5eab48f7addeb5
Nathan <n.sachs.7@gmail.com>
2021-05-16 20:26:37 -0400

    Delay timer blocked out

--- src/components/layout/pages/funnelEditor/steps-types/actions/delay-timer/index.js
+++ src/components/layout/pages/funnelEditor/steps-types/actions/delay-timer/index.js
@@ -15,17 +15,16 @@ import { makeStyles } from "@material-ui/core/styles";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ReplayIcon from "@material-ui/icons/Replay";
 import { withStyles } from '@material-ui/core/styles';
-// import DateFnsUtils from '@date-io/date-fns';
-import {
-  // MuiPickersUtilsProvider,
-  KeyboardTimePicker,
-  // KeyboardDatePicker,
-} from '@material-ui/pickers';
 /**
  * Internal dependencies
  */
-import Tag from "components/svg/Tag/";
-import Toggle from "components/core-ui/toggle";
+ import {
+ 	EMAILS_STORE_NAME
+} from '../../../../../../../data';
+import NewUser from "components/svg/NewUser/";
+import { Toggle } from "components/core-ui/toggle/";
+import { DropDown } from "components/core-ui/drop-down/";
+import { DynamicForm } from "components/core-ui/dynamic-form/";
 import { ACTION, ACTION_TYPE_DEFAULTS } from "../../constants";
 import { registerStepType } from "data/step-type-registry";
 import { createTheme }  from "../../../../../../../theme";
@@ -38,9 +37,14 @@ const useStyles = makeStyles((theme) => ({
     width: "calc(100% - 50px)",
     padding: "33px 25px 18px 25px"
   },
-  inputRow:{
-    margin: '10px 0px 0px 15px'
-  }
+  actionLabel: {
+    color: "#102640",
+    width: "250px",
+    display: "inline-block",
+    marginBottom: "10px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
 }));
 
 const stepAtts = {
@@ -52,46 +56,87 @@ const stepAtts = {
 
   name: "Delay Timer",
 
-  icon: <Tag />,
+  icon: <NewUser />,
+
   read: ({ data, meta, stats }) => {
     return <>Delay Timer</>;
   },
 
   edit: ({ data, meta, stats }) => {
+
+    const timeOptions = [
+      {
+        display: 'Minutes',
+        value: 'minutes'
+      },
+      {
+        display: 'Hours',
+        value: 'hours'
+      },
+      {
+        display: 'Days',
+        value: 'days'
+      },
+      {
+        display: 'Weeks',
+        value: 'weeks'
+      },
+      {
+        display: 'Months',
+        value: 'months'
+      }
+    ]
+    const runOptions = [
+      {
+        display: 'Immediately',
+        value: 'immediately'
+      },
+      {
+        display: 'At time of day',
+        value: 'at-time-of-day'
+      }
+    ]
+
+    const [formData, setFormData] = useState({});
     const classes = useStyles();
 
-    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+    const hanldeFormChange = (e) => {
+      if(e.target.type === 'checkbox'){
+        formData[e.target.id] = e.target.checked
+      } else {
+        formData[e.target.id] = e.target.value
+      }
 
-    const handleDateChange = (date) => {
-      setSelectedDate(date);
-    };
+      setFormData(formData)
+    }
 
-    console.log(data, meta, stats)
 
+
+    const formElements = [
+      {
+      label: 'Wait at least',
+      component: <><input id={'wait-time'} value={formData['wait-time']} type="number" onChange={hanldeFormChange}/><DropDown id={'time-format'} options={timeOptions} value={formData['time-format']} onChange={hanldeFormChange}/>
+      <br/>This role will be added to the new user. If the user already exists, the role will be added in addition to existing roles.</>
+    },
+      {
+      label: 'And run:',
+      component: <><DropDown id={'execution-time'} options={runOptions} value={formData['execution-time']} onChange={hanldeFormChange}/>
+      <br/><Toggle id={'run-in-localtime'} checked={formData['run-in-localtime']} onChange={hanldeFormChange} backgroundColor={theme.palette.primary.main} name="checked" /> Run in the contact's local time.</>
+    },
+      {
+      label: 'Enable conditional logic:',
+      component: <Toggle id={'conditional-logic'} checked={formData['conditional-logic']} onChange={hanldeFormChange} backgroundColor={theme.palette.primary.main} name="checked" />
+    }
+
+  ]
     return <Card className={classes.root}>
+      <div className={classes.actionLabel}>
+        Delay Timer
+      </div>
 
-    <TextField
-      type='time'
-    />
-          </Card>
-    // return <Card className={classes.root}>
-    //
-    // <MuiPickersUtilsProvider utils={DateFnsUtils}>
-    //         <div className={classes.inputRow}>
-    //           <label>Content:</label>
-    //           <KeyboardTimePicker
-    //             margin="normal"
-    //             id="time-picker"
-    //             label="Time picker"
-    //             value={selectedDate}
-    //             onChange={handleDateChange}
-    //             KeyboardButtonProps={{
-    //               'aria-label': 'change time',
-    //             }}
-    //           />
-    //         </div>
-    //       </MuiPickersUtilsProvider>
-    //       </Card>
+      <DynamicForm children={formElements} hanldeFormChange={hanldeFormChange}/>
+
+    </Card>
 
   },
 };
