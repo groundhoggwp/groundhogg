57c2fea965587acda0f82360ea2f7d5781319d29
Corey <corey@Coreys-MacBook-Pro.local>
2021-05-24 12:51:12 -0400

    UI updated

--- src/components/layout/pages/funnelEditor/steps-types/benchmarks/field-changed/index.js
+++ src/components/layout/pages/funnelEditor/steps-types/benchmarks/field-changed/index.js
@@ -2,6 +2,15 @@ import LocalOfferIcon from "@material-ui/icons/LocalOffer";
 import { BENCHMARK } from "../../constants";
 import { registerStepType } from "data/step-type-registry";
 import { BENCHMARK_TYPE_DEFAULTS } from "components/layout/pages/funnels/editor/steps-types/constants";
+import {
+  Card,
+  CardContent,
+  MenuItem,
+  TextField,
+  FormHelperText,
+  Select,
+} from "@material-ui/core";
+import SettingsRow from "components/layout/pages/funnelEditor/components/SettingsRow";
 
 const STEP_TYPE = "field_changed";
 
@@ -20,7 +29,28 @@ const stepAtts = {
   //   return <></>;
   // },
   edit: ({ data, meta, stats }) => {
-    return <></>;
+    return (
+      <Card>
+        <CardContent>
+          <SettingsRow>
+            <TextField
+              label="Which field:"
+              variant="outlined"
+              helperText="Will run when the given field is changed."
+            />
+          </SettingsRow>
+          <SettingsRow>
+            <Select variant="outlined">
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+            </Select>
+            <FormHelperText>When Set To:</FormHelperText>
+          </SettingsRow>
+        </CardContent>
+      </Card>
+    );
   },
 };
 
