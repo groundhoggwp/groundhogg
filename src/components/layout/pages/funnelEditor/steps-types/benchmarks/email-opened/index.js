29a3168dc3f32b53ad5b7a5fad8f580855d7e7eb
Corey <corey@Coreys-MacBook-Pro.local>
2021-05-24 12:04:25 -0400

    Updated with card and settings field

--- src/components/layout/pages/funnelEditor/steps-types/benchmarks/email-opened/index.js
+++ src/components/layout/pages/funnelEditor/steps-types/benchmarks/email-opened/index.js
@@ -3,6 +3,14 @@ import { BENCHMARK } from "../../constants";
 import { registerStepType } from "data/step-type-registry";
 import { BENCHMARK_TYPE_DEFAULTS } from "components/layout/pages/funnels/editor/steps-types/constants";
 
+/**
+ * External dependencies
+ */
+import { Card, CardContent, TextField } from "@material-ui/core";
+import MuiAlert from "@material-ui/lab/Alert";
+import { makeStyles } from "@material-ui/core/styles";
+import SettingsRow from "components/layout/pages/funnelEditor/components/SettingsRow";
+
 const STEP_TYPE = "email_opened";
 
 const stepAtts = {
@@ -20,7 +28,26 @@ const stepAtts = {
   //   return <></>;
   // },
   edit: ({ data, meta, stats }) => {
-    return <></>;
+    return (
+      <Card>
+        <CardContent>
+          <MuiAlert severity="info">
+            Tracking email opens is not always an exact science. Results may
+            vary depending on the email client used by your contacts, false
+            positives and false negatives are possible. We highly recommend you
+            use the Link Clicked benchmark instead. Use with caution.
+          </MuiAlert>
+
+          <SettingsRow>
+            <TextField
+              label="Select email steps:"
+              variant="outlined"
+              helperText="Update the funnel to show new email steps in the email step picker."
+            />
+          </SettingsRow>
+        </CardContent>
+      </Card>
+    );
   },
 };
 
