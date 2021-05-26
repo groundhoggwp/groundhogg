11b8f7c7068d90eb20b774382626d809d2bfc788
Corey <corey@Coreys-MacBook-Pro.local>
2021-05-26 15:02:24 -0400

    add new button at bottom of steps

--- admin/funnels/editor/flow.php
+++ admin/funnels/editor/flow.php
@@ -5,7 +5,7 @@ namespace Groundhogg\Admin\Funnels\Editor;
 ?>
 <div class="step-flow">
 	<div class="steps"></div>
-	<div class="add-new-step">
+	<div class="add-new-step-wrapper">
 		<button class="button button-secondary add-new-step"><?php _e( 'Add new step', 'groundhogg' ) ?></button>
 	</div>
 	<div class="steps-background"></div>
