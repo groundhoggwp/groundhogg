07ba3318d55b5137a73e6e1092aa97f1f64d7827
Corey <corey@Corey.sawtooth.ClaremontMakerSpace.org>
2021-05-27 17:50:04 -0400

    Sticky add new step button

--- admin/funnels/editor/flow.php
+++ admin/funnels/editor/flow.php
@@ -8,6 +8,5 @@ namespace Groundhogg\Admin\Funnels\Editor;
 	<div class="add-new-step-wrapper">
 		<button class="button button-secondary add-new-step"><?php _e( 'Add new step', 'groundhogg' ) ?></button>
 	</div>
-	<div class="steps-background"></div>
 </div>
 
