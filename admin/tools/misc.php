50efac3d42e66bc082bd6b9257835f70fb5017fb
tobeyadr <adrian.methoss@gmail.com>
2021-06-15 11:30:26 -0400

    fix END OF FILE error

--- admin/tools/misc.php
+++ admin/tools/misc.php
@@ -6,21 +6,20 @@ use Groundhogg\Plugin;
 use function Groundhogg\html;
 
 ?>
-	<div id="poststuff">
-		<div class="post-box-grid">
-			<div class="postbox tool">
-				<div class="postbox-header">
-					<h2 class="hndle"><?php _e( 'Refresh optin status tags', 'groundhogg' ); ?></h2>
-				</div>
-				<div class="inside">
-					<p><?php _e( 'In the event the optin status tags of your contacts become out of sync, you can re-sync them using this tool.', 'groundhogg' ); ?></p>
-					<p><?php echo html()->e( 'a', [
-							'class' => 'button',
-							'href'  => Plugin::instance()->tag_mapping->get_start_url(),
-						], __( 'Process', 'groundhogg' ) ) ?></p>
-				</div>
+<div id="poststuff">
+	<div class="post-box-grid">
+		<div class="postbox tool">
+			<div class="postbox-header">
+				<h2 class="hndle"><?php _e( 'Refresh optin status tags', 'groundhogg' ); ?></h2>
 			</div>
-			<?php do_action( 'groundhogg/tools/misc' ); ?>
+			<div class="inside">
+				<p><?php _e( 'In the event the optin status tags of your contacts become out of sync, you can re-sync them using this tool.', 'groundhogg' ); ?></p>
+				<p><?php echo html()->e( 'a', [
+						'class' => 'button',
+						'href'  => Plugin::instance()->tag_mapping->get_start_url(),
+					], __( 'Process', 'groundhogg' ) ) ?></p>
+			</div>
 		</div>
+		<?php do_action( 'groundhogg/tools/misc' ); ?>
 	</div>
-<?php
+</div>
