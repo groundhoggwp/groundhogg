f05056d8c9534438749951644b8307ef7931e3f5
tobeyadr <adrian.methoss@gmail.com>
2021-06-17 11:59:59 -0400

    fix typo

--- api/v4/reports-api.php
+++ api/v4/reports-api.php
@@ -20,7 +20,7 @@ class Reports_Api extends Base_Api {
 			[
 				'methods'              => WP_REST_Server::READABLE,
 				'callback'             => [ $this, 'read' ],
-				'permissions_callback' => [ $this, 'read_permissions_callback' ],
+				'permission_callback' => [ $this, 'read_permissions_callback' ],
 //				'args' => [
 //					'start' => [
 //						'validate_callback' => [ $this, 'is_valid_report_date' ],
@@ -40,7 +40,7 @@ class Reports_Api extends Base_Api {
 			[
 				'methods'              => WP_REST_Server::READABLE,
 				'callback'             => [ $this, 'read_single' ],
-				// 'permissions_callback' => [ $this, 'read_permissions_callback' ],
+				// 'permission_callback' => [ $this, 'read_permissions_callback' ],
 			],
 		] );
 	}
