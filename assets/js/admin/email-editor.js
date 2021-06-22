31b607d46719c8ca518b26dde4ea2e85d2e04170
Corey <corey@Coreys-MacBook-Pro.local>
2021-06-22 12:41:00 -0400

    Header table layout

--- assets/js/admin/email-editor.js
+++ assets/js/admin/email-editor.js
@@ -37,7 +37,7 @@
               <div id="email-editor-content">
                   ${this.components.content.call(this)}
               </div>
-              <details open id="email-editor-controls">
+              <details open id="email-editor-advanced">
                 <Summary>Advanced</Summary>
                   ${this.components.controls.call(this)}
               </details>
@@ -175,7 +175,70 @@
 
       controls() {
         return `
-          <label>Custom headers:</label>
+          <div id="email-editor-advanced-headers">
+            <label>Custom headers:</label>
+            <table>
+              <thead>
+                <tr>
+                  <th>
+                    Key
+                  </th>
+                  <th>Value</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>
+                          <input type="text"/>
+                      </td>
+                      <td>
+                          <input type="text"/>
+                      </td>
+                      <td>
+                        <button class="gh-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
+                        </button>
+                        <button class="gh-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 6l3 18h12l3-18h-18zm19-4v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.316c0 .901.73 2 1.631 2h5.711z"/></svg>
+                        </button>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>
+                          <input type="text"/>
+                      </td>
+                      <td>
+                          <input type="text"/>
+                      </td>
+                      <td>
+                        <button class="gh-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
+                        </button>
+                        <button class="gh-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 6l3 18h12l3-18h-18zm19-4v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.316c0 .901.73 2 1.631 2h5.711z"/></svg>
+                        </button>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>
+                          <input type="text"/>
+                      </td>
+                      <td>
+                          <input type="text"/>
+                      </td>
+                      <td>
+                        <button class="gh-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
+                        </button>
+                        <button class="gh-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 6l3 18h12l3-18h-18zm19-4v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.316c0 .901.73 2 1.631 2h5.711z"/></svg>
+                        </button>
+                      </td>
+                  </tr>
+              </tbody>
+            </table>
+          </div>
         `;
       },
 
