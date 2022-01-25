bfd82825f3947f9b74b1c618879dda8f8d50f0eb
tobeyadr <adrian.methoss@gmail.com>
2022-01-25 12:37:18 -0500

    Allow postbox in postbox in info cards

--- assets/js/admin/info-cards.js
+++ assets/js/admin/info-cards.js
@@ -15,17 +15,17 @@
       stop: saveInfoCardOrder
     })
 
-    $(document).on('click', '.info-cards-wrap button.handlediv', function (e) {
+    $(document).on('click', '.info-cards-wrap .info-card > .postbox-header  button.handlediv', function (e) {
       $(this).closest('.info-card').toggleClass('closed')
       saveInfoCardOrder()
     })
 
-    $(document).on('click', '.info-cards-wrap button.handle-order-higher', function (e) {
+    $(document).on('click', '.info-cards-wrap .info-card > .postbox-header button.handle-order-higher', function (e) {
       $(this).closest('.info-card').insertBefore($(this).closest('.info-card').prev())
       saveInfoCardOrder()
     })
 
-    $(document).on('click', '.info-cards-wrap button.handle-order-lower', function (e) {
+    $(document).on('click', '.info-cards-wrap .info-card > .postbox-header button.handle-order-lower', function (e) {
       $(this).closest('.info-card').insertAfter($(this).closest('.info-card').next())
       saveInfoCardOrder()
     })
@@ -83,42 +83,8 @@
     $(this).closest('.ic-section').toggleClass('open')
   })
 
-  // $(document).on('tinymce-editor-setup', function (event, editor) {
-  //   editor.settings.toolbar1 = 'bold,italic,underline,blockquote,strikethrough,bullist,numlist,alignleft,aligncenter,alignright,undo,redo,link' //Teeny -fullscreen
-  //   editor.settings.height = 200
-  //   editor.on('click', function (ed, e) {
-  //     $(document).trigger('to_mce')
-  //   })
-  // })
-  //
-  // function renderEmailEditor () {
-  //
-  //   setTimeout(function () {
-  //
-  //     wp.editor.initialize(
-  //       'email_content',
-  //       {
-  //         tinymce: true,
-  //         // quicktags: true
-  //       }
-  //     )
-  //   }, 50)
-  // }
-  //
-  // function destroyEmailEditor () {
-  //   wp.editor.remove(
-  //     'email_content'
-  //   )
-  // }
 
   $(function () {
-    // renderEmailEditor()
-
-    // $(document).on('GroundhoggModalContentPulled', destroyEmailEditor)
-    // $(document).on('GroundhoggModalContentPulled', renderEmailEditor)
-    // $(document).on('GroundhoggModalContentPushed', destroyEmailEditor)
-    // $(document).on('GroundhoggModalContentPushed', renderEmailEditor)
-
     init()
   })
 
