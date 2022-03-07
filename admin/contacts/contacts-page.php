40e01d239ba667c62948356f26e09bb0e8c45fbe
Adrian Tobey <adrian.methoss@gmail.com>
2022-03-07 11:02:37 -0500

    change blacklist_keys to disallowed_keys

--- admin/contacts/contacts-page.php
+++ admin/contacts/contacts-page.php
@@ -717,10 +717,10 @@ class Contacts_Page extends Admin_Page {
 				$ip_address = $contact->get_meta( 'ip_address' );
 
 				if ( $ip_address ) {
-					$blacklist = get_option( 'blacklist_keys' );
+					$blacklist = get_option( 'disallowed_keys' );
 					$blacklist .= "\n" . $ip_address;
 					$blacklist = sanitize_textarea_field( $blacklist );
-					update_option( 'blacklist_keys', $blacklist );
+					update_option( 'disallowed_keys', $blacklist );
 				}
 			}
 
