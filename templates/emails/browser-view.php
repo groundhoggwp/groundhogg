99a5bdf9e2f8439f04933b3e7e51e8bfe0da29a3
Adrian Tobey <adrian.methoss@gmail.com>
2023-01-18 18:23:08 -0500

    create a contact record if use is logged in and is admin

--- templates/emails/browser-view.php
+++ templates/emails/browser-view.php
@@ -13,6 +13,20 @@ if ( ! $email ){
 }
 
 $contact = get_contactdata();
+
+if ( ! $contact ){
+
+    // Create a new contact record for the current user if they are an admin
+    if ( is_user_logged_in() && current_user_can( 'add_contacts' ) ){
+        $contact = create_contact_from_user( wp_get_current_user() );
+    }
+
+    // if still no contact, die
+    if ( ! $contact ){
+	    wp_die( 'No contact record available for preview...' );
+    }
+}
+
 $email->set_contact( $contact );
 
 $subject = $email->get_merged_subject_line();
