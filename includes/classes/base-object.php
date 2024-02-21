fdcd7fbf47ca5ce992e3b8bfd6e4bb721a8ce50c
Adrian Tobey <adrian.methoss@gmail.com>
2024-02-21 17:22:31 -0500

    use array_udiff_assoc becuase array_diff_assoc does not handle multidimensional arrays

--- includes/classes/base-object.php
+++ includes/classes/base-object.php
@@ -328,7 +328,15 @@ abstract class Base_Object extends Supports_Errors implements Serializable, Arra
 
 		// Only update different data from the current.
 		$data = $this->sanitize_columns( $data );
-		$data = array_diff_assoc( $data, $this->data );
+
+		// array_diff_assoc only handles 1 dimensional arrays, so we have to serialize stuff that might be serialized later.
+		$data = array_udiff_assoc( $data, $this->data, function ( $a, $b ){
+
+			$a = maybe_serialize( $a );
+			$b = maybe_serialize( $b );
+
+			return $a <=> $b;
+		} );
 
 		// updating with existing data
 		if ( empty( $data ) ) {
