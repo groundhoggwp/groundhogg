15c3e94f89da7681f4e60f7277a3f9dca148c46f
Adrian Tobey <adrian.methoss@gmail.com>
2024-09-25 13:26:30 -0400

    fix import button overlapping tools panel

--- admin/tools/add-import.php
+++ admin/tools/add-import.php
@@ -27,8 +27,8 @@ do_action( 'groundhogg/import/form/before' );
 		<p class="tools-help"><?php _e( 'If you have a .CSV file you can upload it here!', 'groundhogg' ); ?></p>
 		<form method="post" enctype="multipart/form-data" class="gh-tools-box gh-panel">
 			<?php wp_nonce_field(); ?>
-            <div class="display-flex">
-                <input type="file" name="import_file" id="import_file" accept=".csv">
+            <div class="display-flex align-center flex-wrap">
+                <input type="file" name="import_file" id="import_file" accept=".csv" style="width: 200px; margin-right: auto">
                 <button class="gh-button primary" name="import_file_button"
                         value="import"><?php _ex( 'Import Contacts', 'action', 'groundhogg' ); ?></button>
             </div>
