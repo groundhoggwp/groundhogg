a7b067ad6a7d1f73c98ba4a40fd18fa872205a2c
Adrian Tobey <adrian.methoss@gmail.com>
2024-11-14 15:41:27 -0500

    remove unused imports

--- includes/background/sync-users.php
+++ includes/background/sync-users.php
@@ -2,26 +2,12 @@
 
 namespace Groundhogg\background;
 
-use Groundhogg\Contact;
-use Groundhogg\Contact_Query;
-use Groundhogg\Preferences;
 use function Groundhogg\_nf;
 use function Groundhogg\bold_it;
-use function Groundhogg\code_it;
-use function Groundhogg\contact_filters_link;
-use function Groundhogg\count_csv_rows;
 use function Groundhogg\create_contact_from_user;
-use function Groundhogg\files;
-use function Groundhogg\generate_contact_with_map;
-use function Groundhogg\get_array_var;
-use function Groundhogg\get_csv_delimiter;
-use function Groundhogg\is_a_contact;
 use function Groundhogg\is_option_enabled;
-use function Groundhogg\isset_not_empty;
 use function Groundhogg\notices;
 use function Groundhogg\percentage;
-use function Groundhogg\track_activity;
-use function Groundhogg\white_labeled_name;
 
 class Sync_Users extends Task {
 
@@ -31,9 +17,9 @@ class Sync_Users extends Task {
 
 	const BATCH_LIMIT = 100;
 
-	public function __construct(int $batch = 0 ) {
-		$this->user_id  = get_current_user_id();
-		$this->batch    = $batch;
+	public function __construct( int $batch = 0 ) {
+		$this->user_id = get_current_user_id();
+		$this->batch   = $batch;
 
 		$user_count = count_users();
 		$num_users  = $user_count['total_users'];
@@ -54,7 +40,7 @@ class Sync_Users extends Task {
 	 *
 	 * @return string
 	 */
-	public function get_title(){
+	public function get_title() {
 		return sprintf( 'Sync %s users', bold_it( _nf( $this->users ) ) );
 	}
 
@@ -84,14 +70,15 @@ class Sync_Users extends Task {
 		if ( empty( $users ) ) {
 			$message = sprintf( __( '%s users have been synced!', 'groundhogg' ), bold_it( _nf( $this->users ) ) );
 			notices()->add_user_notice( $message, 'success', true, $this->user_id );
+
 			return true;
 		}
 
-		foreach ( $users as $user ){
+		foreach ( $users as $user ) {
 			create_contact_from_user( $user, is_option_enabled( 'gh_sync_user_meta' ) );
 		}
 
-		$this->batch++;
+		$this->batch ++;
 
 		return false;
 	}
