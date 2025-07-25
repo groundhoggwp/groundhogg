9c6db3389a6eb5a2f2204d41d127c5bbaebdf213
Adrian Tobey <adrian.methoss@gmail.com>
2025-07-25 13:14:50 -0400

    steps store

--- assets/js/admin/data.js
+++ assets/js/admin/data.js
@@ -882,6 +882,7 @@
           sort((a, b) => a.data.step_order - b.data.step_order)
       },
     }),
+    steps: ObjectStore(Groundhogg.api.routes.v4.steps),
     emails: ObjectStore(Groundhogg.api.routes.v4.emails, {
       send (id, data) {
         return apiPost(`${ this.route }/${ id }/send`, data)
